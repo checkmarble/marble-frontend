06471da4f8bea4b9d6a823b2b75e5611bada72db
Thomas Lathuiliere <thomas@checkmarble.com>
2023-03-07 18:23:34 +0100

    feat(trigger): add scenario id

--- apps/builder/app/routes/__builder/scenarios/$scenarioId/i/$incrementId/view/trigger.tsx
+++ apps/builder/app/routes/__builder/scenarios/$scenarioId/i/$incrementId/view/trigger.tsx
@@ -5,21 +5,74 @@ import { LogicalOperator } from '@marble-front/builder/components/Scenario/Logic
 import { triggerFixture } from '@marble-front/builder/fixtures/trigger';
 import clsx from 'clsx';
 import { Fragment } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import { useCurrentScenarioIncrement } from '../../$incrementId';
+import { toast } from 'react-hot-toast';
 
 export const handle = {
-  i18n: ['scenarios'] as const,
+  i18n: ['scenarios', 'common'] as const,
 };
 
 export default function Trigger() {
   const { t } = useTranslation(handle.i18n);
 
+  const { id } = useCurrentScenarioIncrement();
+
   const trigger = triggerFixture.complex;
 
   return (
     <Paper.Container>
-      <Paper.Title>{t('scenarios:trigger.trigger_object.title')}</Paper.Title>
-      <Callout>{t('scenarios:trigger.trigger_object.callout')}</Callout>
+      <div className="flex flex-col gap-2 lg:gap-4">
+        <Paper.Title>{t('scenarios:trigger.run_scenario.title')}</Paper.Title>
+        <p className="text-s text-grey-100 font-normal">
+          <Trans
+            t={t}
+            i18nKey="scenarios:trigger.run_scenario.description.docs"
+            components={{
+              DocLink: (
+                // eslint-disable-next-line jsx-a11y/anchor-has-content
+                <a
+                  className="text-purple-100"
+                  href="https://docs.checkmarble.com/reference/introduction-1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+              ),
+            }}
+          />
+          <br />
+          <Trans
+            t={t}
+            i18nKey="scenarios:trigger.run_scenario.description.scenario_id"
+            components={{
+              ScenarioIdLabel: <code className="select-none" />,
+              ScenarioIdValue: (
+                <code
+                  className="border-grey-10 cursor-pointer select-none rounded-sm border px-1"
+                  onClick={() => {
+                    navigator.clipboard.writeText(id).then(() =>
+                      toast.success(
+                        t('common:clipboard.copy', {
+                          ns: 'common',
+                          replace: { value: id },
+                        })
+                      )
+                    );
+                  }}
+                />
+              ),
+            }}
+            values={{
+              scenarioId: id,
+            }}
+          />
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2 lg:gap-4">
+        <Paper.Title>{t('scenarios:trigger.trigger_object.title')}</Paper.Title>
+        <Callout>{t('scenarios:trigger.trigger_object.callout')}</Callout>
+      </div>
 
       <div className="text-s grid grid-cols-[8px_16px_max-content_1fr]">
         <ScenarioBox className="bg-grey-02 col-span-4 w-fit font-semibold text-purple-100">
