18935291880c293a591f01fc8c07215535f34f58
Thomas Lathuiliere <thomas@checkmarble.com>
2023-05-09 16:57:59 +0200

    feat(rule): branch rule view on new api (w/ formula)

--- apps/builder/app/routes/__builder/scenarios/$scenarioId/i/$incrementId/view.rules.$ruleId.tsx
+++ apps/builder/app/routes/__builder/scenarios/$scenarioId/i/$incrementId/view.rules.$ruleId.tsx
@@ -1,43 +1,47 @@
-// import {
-//   Callout,
-//   Rule,
-//   scenarioI18n,
-//   ScenarioPage,
-// } from '@marble-front/builder/components';
-// import { fromUUID, toUUID } from '@marble-front/builder/utils/short-uuid';
-// import { Link, useParams } from '@remix-run/react';
-// import { type Namespace } from 'i18next';
-// import invariant from 'tiny-invariant';
+import { getScenarioIterationRule } from '@marble-front/api/marble';
+import {
+  Callout,
+  Rule,
+  scenarioI18n,
+  ScenarioPage,
+} from '@marble-front/builder/components';
+import { authenticator } from '@marble-front/builder/services/auth/auth.server';
+import { fromParams, fromUUID } from '@marble-front/builder/utils/short-uuid';
+import { json, type LoaderArgs } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import { type Namespace } from 'i18next';
 
-// import { useCurrentScenarioIncrement } from '../$incrementId';
+export const handle = {
+  i18n: [...scenarioI18n] satisfies Namespace,
+};
 
-// export const handle = {
-//   i18n: [...scenarioI18n] satisfies Namespace,
-// };
+export async function loader({ request, params }: LoaderArgs) {
+  await authenticator.isAuthenticated(request, {
+    failureRedirect: '/login',
+  });
 
-// export default function RuleView() {
-//   const {
-//     body: { rules },
-//   } = useCurrentScenarioIncrement();
+  const ruleId = fromParams(params, 'ruleId');
 
-//   const { ruleId } = useParams();
-//   invariant(ruleId, 'ruleId is required');
+  const rule = await getScenarioIterationRule(ruleId);
 
-//   const rule = rules.find(({ id }) => id === toUUID(ruleId));
-//   invariant(rule, `Unknown rule`);
+  return json(rule);
+}
 
-//   return (
-//     <ScenarioPage.Container>
-//       <ScenarioPage.Header>
-//         <Link to="./.." className="mr-4">
-//           <ScenarioPage.BackButton />
-//         </Link>
-//         {rule.name ?? fromUUID(rule.id)}
-//       </ScenarioPage.Header>
-//       <ScenarioPage.Content>
-//         <Callout>{rule.description}</Callout>
-//         <Rule rule={rule} />
-//       </ScenarioPage.Content>
-//     </ScenarioPage.Container>
-//   );
-// }
+export default function RuleView() {
+  const rule = useLoaderData<typeof loader>();
+
+  return (
+    <ScenarioPage.Container>
+      <ScenarioPage.Header>
+        <Link to="./.." className="mr-4">
+          <ScenarioPage.BackButton />
+        </Link>
+        {rule.name ?? fromUUID(rule.id)}
+      </ScenarioPage.Header>
+      <ScenarioPage.Content>
+        <Callout>{rule.description}</Callout>
+        <Rule rule={rule} />
+      </ScenarioPage.Content>
+    </ScenarioPage.Container>
+  );
+}
