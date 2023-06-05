bb9cae4933860a3c9a108668c2d82e0d255dc13a
Thomas Lathuiliere <thomas@checkmarble.com>
2023-06-05 11:12:57 +0200

    fix(logger): redirect to stdout

--- apps/builder/app/services/logger/index.ts
+++ apps/builder/app/services/logger/index.ts
@@ -1,9 +1,9 @@
 import { LoggingWinston } from '@google-cloud/logging-winston';
 import winston from 'winston';
 
-const loggingWinston = new LoggingWinston();
+const loggingWinston = new LoggingWinston({ redirectToStdout: true });
 
 export const logger = winston.createLogger({
   level: 'info',
-  transports: [new winston.transports.Console(), loggingWinston],
+  transports: [loggingWinston],
 });
