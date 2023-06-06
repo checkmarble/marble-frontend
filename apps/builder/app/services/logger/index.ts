5f6cb6ba118bf90559fe9e816e7d1da727ea0e55
Thomas Lathuiliere <thomas@checkmarble.com>
2023-06-06 18:35:39 +0200

    refactor(logger): use custom dev logger

--- apps/builder/app/services/logger/index.ts
+++ apps/builder/app/services/logger/index.ts
@@ -1,9 +1,18 @@
 import { LoggingWinston } from '@google-cloud/logging-winston';
 import winston from 'winston';
 
-const loggingWinston = new LoggingWinston({ redirectToStdout: true });
+const loggingWinston = new LoggingWinston({
+  redirectToStdout: true,
+});
 
 export const logger = winston.createLogger({
   level: 'info',
-  transports: [loggingWinston],
+  transports: [
+    // eslint-disable-next-line no-restricted-properties
+    process.env.NODE_ENV === 'development'
+      ? new winston.transports.Console({
+          format: winston.format.prettyPrint({ colorize: true }),
+        })
+      : loggingWinston,
+  ],
 });
