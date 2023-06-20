bfffa5b41f8bb68904ce0cbad7f6261d121c1691
Thomas Lathuiliere <thomas@checkmarble.com>
2023-06-20 18:16:36 +0200

    feat(scenario): add new formula component

--- apps/builder/app/components/Scenario/Formula/Operators/Math.tsx
+++ apps/builder/app/components/Scenario/Formula/Operators/Math.tsx
@@ -1,10 +1,14 @@
-import { type MathOperator as MathOperatorType } from '@marble-front/operators';
+import {
+  type MathOperator as MathOperatorType,
+  type MathOperatorNode,
+  type MathOperatorNodeName,
+} from '@marble-front/operators';
 import { assertNever } from '@marble-front/typescript-utils';
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { scenarioI18n } from '../../scenario-i18n';
-import { Formula } from '../Formula';
+import { Formula, NewFormula } from '../Formula';
 import { Condition } from './Condition';
 
 interface MathProps {
@@ -83,6 +87,75 @@ function MathOperator({
   return (
     <span className="text-grey-100 font-semibold">
       {getOperatorLabel(operatorType)}
+    </span>
+  );
+}
+
+interface NewMathProps {
+  node: MathOperatorNode;
+  isRoot?: boolean;
+}
+
+export function NewMath({ node, isRoot }: NewMathProps) {
+  const operatorName = node.operatorName;
+  let operands;
+  if (operatorName === 'STRING_IS_IN_LIST') {
+    operands = [node.namedChildren.value, node.namedChildren.list];
+  } else {
+    operands = node.children;
+  }
+  return (
+    <Condition.Container isRoot={isRoot}>
+      {operands.map((operand, index) => {
+        return (
+          <React.Fragment key={`${operand.operatorName}-${index}`}>
+            {index !== 0 && (
+              <Condition.Item className="px-4" isRoot={isRoot}>
+                <NewMathOperator operatorName={operatorName} />
+              </Condition.Item>
+            )}
+            <Condition.Item isRoot={isRoot}>
+              <NewFormula node={operand} />
+            </Condition.Item>
+          </React.Fragment>
+        );
+      })}
+    </Condition.Container>
+  );
+}
+
+// Function instead of obejct mapping to handle possible translation (ex: "IS IN" operator)
+function useGetNewOperatorLabel() {
+  const { t } = useTranslation(scenarioI18n);
+
+  return useCallback(
+    (operatorName: MathOperatorNodeName) => {
+      switch (operatorName) {
+        case 'AND':
+          return '×';
+        case 'OR':
+          return '+';
+        case 'GREATER':
+          return '>';
+        case 'STRING_IS_IN_LIST':
+          return t('scenarios:operator.is_in');
+        default:
+          assertNever('unknwon Math operator :', operatorName);
+      }
+    },
+    [t]
+  );
+}
+
+function NewMathOperator({
+  operatorName,
+}: {
+  operatorName: MathOperatorNodeName;
+}) {
+  const getOperatorLabel = useGetNewOperatorLabel();
+  return (
+    <span className="text-grey-100 font-semibold">
+      {getOperatorLabel(operatorName)}
     </span>
   );
 }
