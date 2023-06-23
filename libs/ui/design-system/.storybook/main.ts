aaced994a4e0d499766c5df10e96307b6ac73c7c
Thomas Lathuiliere <thomas@checkmarble.com>
2023-06-23 15:03:42 +0200

    fix(storybook): fix storybook tsconfig alias path

--- libs/ui/design-system/.storybook/main.ts
+++ libs/ui/design-system/.storybook/main.ts
@@ -18,13 +18,7 @@ const config: StorybookConfig = {
   },
   async viteFinal(config) {
     return mergeConfig(config, {
-      // For now, this seems bugged with Storybook (so we need to specify manually resolve.alias)
       plugins: [viteTsConfigPaths()],
-      resolve: {
-        alias: {
-          '@marble-front/ui/icons': 'libs/ui/icons/src/index.ts',
-        },
-      },
     });
   },
 };
