ff4e6338666b7a9743ff4af3680c6cade1c9c99e
Vivien <vivien@checkmarble.com>
2023-09-04 18:56:08 +0200

    ci: add support of vite in package app-builder

---
+++ packages/app-builder/vitest.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { defineProject } from 'vitest/config';
+
+export default defineProject({
+  plugins: [react(), viteTsConfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
