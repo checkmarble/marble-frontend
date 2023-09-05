41f51b7ea1e3d929462d171004e767bf78e88cb8
Vivien <vivien@checkmarble.com>
2023-09-05 09:16:04 +0200

    test: findAndReplaceNode

--- packages/app-builder/src/services/editor/ast-editor.ts
+++ packages/app-builder/src/services/editor/ast-editor.ts
@@ -13,6 +13,8 @@ import { nanoid } from 'nanoid';
 import { useCallback, useEffect, useState } from 'react';
 import * as R from 'remeda';
 
+import { findAndReplaceNode } from './FindAndReplaceNode';
+
 // TODO: trancher entre Builder vs Editor
 export interface EditorNodeViewModel {
   nodeId: string;
@@ -233,41 +235,6 @@ export function useAstBuilder({
     appendChild,
     remove,
     save,
-  };
-}
-
-// Find the node `nodeIdToReplace` by walking the tree bottom-up
-// When found, apply `fn` to the node and replace the node with the result
-// If fn return null, the node is removed.
-function findAndReplaceNode(
-  nodeIdToReplace: string,
-  fn: (node: EditorNodeViewModel) => EditorNodeViewModel | null,
-  node: EditorNodeViewModel
-): EditorNodeViewModel | null {
-  if (node.nodeId === nodeIdToReplace) {
-    return fn(node);
-  }
-
-  const children = R.pipe(
-    node.children,
-    R.map((child) => findAndReplaceNode(nodeIdToReplace, fn, child)),
-    R.compact
-  );
-
-  const namedChildren = R.pipe(
-    R.toPairs(node.namedChildren),
-    R.map(([key, child]) => {
-      const newChild = findAndReplaceNode(nodeIdToReplace, fn, child);
-      return newChild === null ? null : ([key, newChild] as const);
-    }),
-    R.compact,
-    (pairs) => R.fromPairs(pairs)
-  );
-
-  return {
-    ...node,
-    children,
-    namedChildren,
   };
 }
 
