c210d10da94013e2e834de115336cb62ccccece1
Vivien <vivien@checkmarble.com>
2023-09-14 16:34:25 +0200

    fix: openapi LinkToSingleDto

--- packages/app-builder/src/models/data-model.ts
+++ packages/app-builder/src/models/data-model.ts
@@ -3,13 +3,17 @@ import {
   type DataModelFieldDto,
   type LinkToSingleDto,
 } from '@marble-api';
+import * as R from 'remeda';
 
 export interface DataModelField {
   name: string;
   dataType: 'Bool' | 'Int' | 'Float' | 'String' | 'Timestamp' | 'unknown';
+  description: string;
+  nullable: boolean;
 }
 
 export interface LinksToSingle {
+  linkName: string;
   linkedTableName: string;
   parentFieldName: string;
   childFieldName: string;
@@ -24,42 +28,38 @@ export interface DataModel {
 function adaptFieldDto(dataModelFieldsDto: {
   [key: string]: DataModelFieldDto;
 }): DataModelField[] {
-  return Object.entries(dataModelFieldsDto).map(
-    ([name, field]): DataModelField => ({
+  return R.pipe(
+    R.toPairs(dataModelFieldsDto),
+    R.map(([name, field]) => ({
       name: name,
-      dataType: field.dataType,
-    })
+      dataType: field.data_type,
+      description: field.description,
+      nullable: field.nullable,
+    }))
   );
 }
 
 function adaptLinkToSingleDto(linksToSingleDto: {
   [key: string]: LinkToSingleDto;
 }): LinksToSingle[] {
-  const linkToSingle: LinksToSingle[] = [];
-  Object.keys(linksToSingleDto).forEach((key) => {
-    linkToSingle.push({
-      linkedTableName: linksToSingleDto[key].linkedTableName,
-      parentFieldName: linksToSingleDto[key].parentFieldName,
-      childFieldName: linksToSingleDto[key].childFieldName,
-    });
-  });
-  return linkToSingle;
+  return R.pipe(
+    R.toPairs(linksToSingleDto),
+    R.map(([linkName, linkToSingleDto]) => ({
+      linkName,
+      linkedTableName: linkToSingleDto.linked_table_name,
+      parentFieldName: linkToSingleDto.parent_field_name,
+      childFieldName: linkToSingleDto.child_field_name,
+    }))
+  );
 }
 
 export function adaptDataModelDto(dataModelDto: DataModelDto): DataModel[] {
-  const dataModel: DataModel[] = [];
-  Object.keys(dataModelDto.tables).forEach((key) => {
-    let linksToSingle: LinksToSingle[] = [];
-    if (dataModelDto.tables[key].links_to_single !== undefined) {
-      linksToSingle = adaptLinkToSingleDto(
-        dataModelDto.tables[key].links_to_single ?? {}
-      );
-    }
-    dataModel.push({
-      name: dataModelDto.tables[key].name,
-      fields: adaptFieldDto(dataModelDto.tables[key].fields),
-      linksToSingle: linksToSingle,
-    });
-  });
-  return dataModel;
+  return R.pipe(
+    R.toPairs(dataModelDto.tables),
+    R.map(([tableName, tableDto]) => ({
+      name: tableName,
+      fields: adaptFieldDto(tableDto.fields),
+      linksToSingle: adaptLinkToSingleDto(tableDto.links_to_single ?? {}),
+    }))
+  );
 }
