0c9e796543b351c88331bc9f118e268d7c1a11e9
Thomas Lathuiliere <thomas@checkmarble.com>
2023-09-29 18:13:52 +0200

    feat(edit/view): introduce edit-view

---
+++ packages/app-builder/src/routes/__builder/scenarios/$scenarioId/i/$iterationId/edit-view/index.tsx
@@ -0,0 +1,10 @@
+import { serverServices } from '@app-builder/services/init.server';
+import { type LoaderArgs } from '@remix-run/node';
+
+export async function loader({ request }: LoaderArgs) {
+  const { authService } = serverServices;
+  return authService.isAuthenticated(request, {
+    successRedirect: './trigger',
+    failureRedirect: '/login',
+  });
+}
