911e1abdad6a02787591bc8444cfebf67fda3dbc
Pascal Delange <pascal@checkmarble.com>
2023-09-29 17:06:00 +0200

    add proper icon

--- packages/app-builder/src/routes/__builder.tsx
+++ packages/app-builder/src/routes/__builder.tsx
@@ -11,8 +11,8 @@ import { Form, Outlet, useLoaderData } from '@remix-run/react';
 import { Button, ScrollArea } from '@ui-design-system';
 import {
   Arrow2Down,
-  Database,
   Decision,
+  Harddrive,
   Lists,
   LogoStandard,
   Logout,
@@ -41,7 +41,7 @@ const LINKS: SidebarLinkProps[] = [
 ];
 
 const BOTTOM_LINKS: SidebarLinkProps[] = [
-  { labelTKey: 'navigation:data', to: getRoute('/data'), Icon: Database },
+  { labelTKey: 'navigation:data', to: getRoute('/data'), Icon: Harddrive },
 ];
 
 export async function loader({ request }: LoaderArgs) {
