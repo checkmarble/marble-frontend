4d5f89ef8210c91e18ad5bb2d1b565ba28320b3d
Zoé Cadé <zoe.cade@gmail.com>
2023-10-13 12:34:25 +0200

    feat: create new rule with default node

--- packages/app-builder/src/components/Scenario/AstBuilder/RootAstBuilderNode/RootAnd.tsx
+++ packages/app-builder/src/components/Scenario/AstBuilder/RootAstBuilderNode/RootAnd.tsx
@@ -79,64 +79,65 @@ export function RootAnd({
    *
    */
   return (
-    <div className="text-s grid grid-cols-[8px_16px_max-content_1fr]">
-      <div className="text-s bg-grey-02 col-span-4 flex h-fit min-h-[40px] w-fit min-w-[40px] flex-wrap items-center justify-center gap-1 rounded p-2 font-semibold text-purple-100">
-        {builder.triggerObjectTable.name}
-      </div>
-      {rootAndViewModel.children.map((child, childIndex) => {
-        const isFirstCondition = childIndex === 0;
-        const isLastCondition =
-          childIndex === rootAndViewModel.children.length - 1;
+    <>
+      <div className="text-s grid grid-cols-[8px_16px_max-content_1fr]">
+        <div className="text-s bg-grey-02 col-span-4 flex h-fit min-h-[40px] w-fit min-w-[40px] flex-wrap items-center justify-center gap-1 rounded p-2 font-semibold text-purple-100">
+          {builder.triggerObjectTable.name}
+        </div>
+        {rootAndViewModel.children.map((child, childIndex) => {
+          const isFirstCondition = childIndex === 0;
+          const isLastCondition =
+            childIndex === rootAndViewModel.children.length - 1;
 
-        return (
-          <Fragment key={`condition_${child.nodeId}`}>
-            {/* Row 1 */}
-            <div
-              className={clsx(
-                'border-grey-10 col-span-4 w-2 border-r ',
-                isFirstCondition ? 'h-4' : 'h-2'
-              )}
-            />
+          return (
+            <Fragment key={`condition_${child.nodeId}`}>
+              {/* Row 1 */}
+              <div
+                className={clsx(
+                  'border-grey-10 col-span-4 w-2 border-r ',
+                  isFirstCondition ? 'h-4' : 'h-2'
+                )}
+              />
 
-            {/* Row 2 */}
-            <div
-              className={clsx(
-                'border-grey-10 border-r',
-                isLastCondition && 'h-5'
-              )}
-            />
-            <div className="border-grey-10 h-5 border-b" />
-            <LogicalOperatorLabel
-              className="bg-grey-02 mr-2 p-2"
-              operator={isFirstCondition ? 'where' : 'and'}
-              color={andChildrenErrors.length > 0 ? 'red' : 'grey'}
-            />
+              {/* Row 2 */}
+              <div
+                className={clsx(
+                  'border-grey-10 border-r',
+                  isLastCondition && 'h-5'
+                )}
+              />
+              <div className="border-grey-10 h-5 border-b" />
+              <LogicalOperatorLabel
+                className="bg-grey-02 mr-2 p-2"
+                operator={isFirstCondition ? 'where' : 'and'}
+                color={andChildrenErrors.length > 0 ? 'red' : 'grey'}
+              />
 
-            <div className="flex items-center gap-2">
-              <div className="flex flex-1 flex-col">
-                <AstBuilderNode
-                  builder={builder}
-                  editorNodeViewModel={child}
-                  viewOnly={viewOnly}
-                />
+              <div className="flex items-center gap-2">
+                <div className="flex flex-1 flex-col">
+                  <AstBuilderNode
+                    builder={builder}
+                    editorNodeViewModel={child}
+                    viewOnly={viewOnly}
+                  />
+                </div>
+                {!viewOnly && (
+                  <RemoveButton
+                    className="peer"
+                    onClick={() => {
+                      builder.remove(child.nodeId);
+                    }}
+                  />
+                )}
               </div>
-              {!viewOnly && (
-                <RemoveButton
-                  className="peer"
-                  onClick={() => {
-                    builder.remove(child.nodeId);
-                  }}
-                />
-              )}
-            </div>
-          </Fragment>
-        );
-      })}
+            </Fragment>
+          );
+        })}
 
-      {!viewOnly && (
-        <AddLogicalOperatorButton onClick={appendAndChild} operator="and" />
-      )}
-
+        {!viewOnly && (
+          <AddLogicalOperatorButton onClick={appendAndChild} operator="and" />
+        )}
+      </div>
       <div className="flex flex-row flex-wrap gap-2">
         {andNonChildrenErrors.map((error, index) => (
           <ScenarioValidationError key={index}>
@@ -144,6 +145,6 @@ export function RootAnd({
           </ScenarioValidationError>
         ))}
       </div>
-    </div>
+    </>
   );
 }
--- packages/app-builder/src/routes/ressources/scenarios/$scenarioId/$iterationId/rules/create.tsx
+++ packages/app-builder/src/routes/ressources/scenarios/$scenarioId/$iterationId/rules/create.tsx
@@ -1,3 +1,4 @@
+import { adaptNodeDto, NewEmptyRuleAstNode } from '@app-builder/models';
 import { serverServices } from '@app-builder/services/init.server';
 import { getRoute } from '@app-builder/utils/routes';
 import { fromParams, fromUUID } from '@app-builder/utils/short-uuid';
@@ -25,7 +26,7 @@ export async function action({ request, params }: ActionArgs) {
     const { rule } = await apiClient.createScenarioIterationRule({
       scenarioIterationId: iterationId,
       displayOrder: 1,
-      formula_ast_expression: null,
+      formula_ast_expression: adaptNodeDto(NewEmptyRuleAstNode()),
       name: t('create_rule.default_name'),
       description: '',
       scoreModifier: 0,
