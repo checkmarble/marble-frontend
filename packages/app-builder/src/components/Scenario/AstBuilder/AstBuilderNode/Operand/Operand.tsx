4724dea70545e8ae858b5022ecf97636091f498d
Zoé Cadé <zoe.cade@gmail.com>
2023-10-16 11:57:41 +0200

    feat: display function errors

--- packages/app-builder/src/components/Scenario/AstBuilder/AstBuilderNode/Operand/Operand.tsx
+++ packages/app-builder/src/components/Scenario/AstBuilder/AstBuilderNode/Operand/Operand.tsx
@@ -1,6 +1,8 @@
 import {
   adaptLabelledAst,
   type AstNode,
+  type EvaluationError,
+  functionNodeNames,
   isAggregation,
   isConstant,
   isCustomListAccess,
@@ -13,12 +15,25 @@ import {
   adaptAstNodeFromEditorViewModel,
   type AstBuilder,
   type EditorNodeViewModel,
+  flattenViewModelErrors,
 } from '@app-builder/services/editor/ast-editor';
 
 import { Default } from '../Default';
 import { OperandEditor } from './OperandEditor';
 
 export type OperandViewModel = EditorNodeViewModel;
+
+export const computeOperandErrors = (
+  vm: EditorNodeViewModel
+): EvaluationError[] => {
+  if (vm.funcName && functionNodeNames.includes(vm.funcName)) {
+    return flattenViewModelErrors(vm).length > 0
+      ? [{ error: 'FUNCTION_ERROR', message: 'function has error' }]
+      : [];
+  } else {
+    return flattenViewModelErrors(vm);
+  }
+};
 
 export function isEditableOperand(node: AstNode): boolean {
   return (
