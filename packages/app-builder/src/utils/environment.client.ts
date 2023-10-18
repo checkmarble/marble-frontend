be868714bca41242d62671a1a9debed52c22475e
Zoé Cadé <zoe.cade@gmail.com>
2023-10-18 17:37:14 +0200

    Revert "POC Sentry (#206)"
    This reverts commit 99df07e3b5fd25e0c5dd0427214c0ee846adbfe6.

--- packages/app-builder/src/utils/environment.client.ts
+++ packages/app-builder/src/utils/environment.client.ts
@@ -11,9 +11,6 @@ export type ClientEnvVars = {
   AUTH_EMULATOR_HOST?: string;
   FIREBASE_OPTIONS: FirebaseOptions;
   MARBLE_API_DOMAIN: string;
-  MARBLE_APP_DOMAIN: string;
-  SENTRY_DSN: string;
-  SENTRY_ENVIRONMENT: string;
 };
 
 export function getClientEnv<K extends keyof ClientEnvVars>(
