cca7e73b84596b70add3a2b7ed5f5182cd145476
Thomas Lathuiliere <thomas@checkmarble.com>
2023-11-07 18:49:56 +0100

    chore(api): add filtering to listDecisions

--- packages/app-builder/src/routes/__builder/decisions/index.tsx
+++ packages/app-builder/src/routes/__builder/decisions/index.tsx
@@ -7,9 +7,10 @@ import {
 } from '@app-builder/components';
 import { decisionFilters } from '@app-builder/components/Decisions/Filters/filters';
 import { serverServices } from '@app-builder/services/init.server';
+import { parseQuerySafe } from '@app-builder/utils/input-validation';
 import { getRoute } from '@app-builder/utils/routes';
 import { fromUUID } from '@app-builder/utils/short-uuid';
-import { json, type LoaderArgs } from '@remix-run/node';
+import { json, type LoaderArgs, redirect } from '@remix-run/node';
 import { Form, useLoaderData, useRouteError } from '@remix-run/react';
 import { captureRemixErrorBoundaryError } from '@sentry/remix';
 import { type Namespace } from 'i18next';
@@ -17,10 +18,19 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Input } from 'ui-design-system';
 import { Decision, Filters, Search } from 'ui-icons';
+import * as z from 'zod';
 
 export const handle = {
   i18n: ['common', 'navigation', ...decisionsI18n] satisfies Namespace,
 };
+
+const decisionFiltersSchema = z.object({
+  outcome: z.array(z.enum(['approve', 'review', 'decline'])).optional(),
+  triggerObject: z.array(z.string()).optional(),
+  startDate: z.string().optional(),
+  endDate: z.string().optional(),
+  scenarioId: z.array(z.string()).optional(),
+});
 
 export async function loader({ request }: LoaderArgs) {
   const { authService } = serverServices;
@@ -28,16 +38,22 @@ export async function loader({ request }: LoaderArgs) {
     failureRedirect: '/login',
   });
 
-  const decisions = await apiClient.listDecisions();
+  const parsedQuery = await parseQuerySafe(request, decisionFiltersSchema);
+  if (!parsedQuery.success) {
+    return redirect(getRoute('/decisions'));
+  }
+
+  const decisions = await apiClient.listDecisions(parsedQuery.data);
 
   return json({
     decisions,
+    filters: parsedQuery.data,
   });
 }
 
 export default function Decisions() {
   const { t } = useTranslation(handle.i18n);
-  const { decisions } = useLoaderData<typeof loader>();
+  const { decisions, filters } = useLoaderData<typeof loader>();
   const [decisionId, setDecisionId] = useState<string | null>(null);
 
   const decisionIdToParams = (decisionId: string | null) => {
