c09ea884d8595f4ece5ef4fb1e64e4e98f545f89
Thomas Lathuiliere <thomas@checkmarble.com>
2023-12-14 15:50:10 +0100

    feat(ui-design-system): add intermediate checkbox

--- packages/ui-design-system/src/Checkbox/Checkbox.tsx
+++ packages/ui-design-system/src/Checkbox/Checkbox.tsx
@@ -1,29 +1,41 @@
 import { type CheckboxProps, Indicator, Root } from '@radix-ui/react-checkbox';
-import clsx from 'clsx';
+import { cva } from 'class-variance-authority';
 import { forwardRef } from 'react';
-import { Tick } from 'ui-icons';
+import { CheckIndeterminateSmall, Tick } from 'ui-icons';
+
+const checkbox = cva(
+  'disabled:bg-grey-10 bg-grey-00 hover:bg-purple-05 radix-state-checked:border-none radix-state-checked:bg-purple-100 text-grey-00 flex h-6 w-6 shrink-0 items-center justify-center rounded border text-[24px] outline-none',
+  {
+    variants: {
+      color: {
+        purple: 'border-purple-50 focus:border-purple-100',
+        red: 'focus:border-red-120 border-red-100',
+      },
+    },
+  },
+);
 
 export const Checkbox = forwardRef<
   HTMLButtonElement,
   Omit<CheckboxProps, 'asChild'> & {
     color?: 'purple' | 'red';
   }
->(function Checkbox({ className, color = 'purple', ...props }, ref) {
+>(function Checkbox({ className, color = 'purple', checked, ...props }, ref) {
   return (
     <Root
       ref={ref}
-      className={clsx(
-        'disabled:bg-grey-10 bg-grey-00 hover:bg-purple-05 radix-state-checked:border-none radix-state-checked:bg-purple-100 text-grey-00 flex h-6 w-6 shrink-0 items-center justify-center rounded border text-[24px] outline-none',
-        {
-          'border-purple-50 focus:border-purple-100': color === 'purple',
-          'focus:border-red-120 border-red-100': color === 'red',
-        },
-        className,
-      )}
+      className={checkbox({ color, className })}
+      checked={checked}
       {...props}
     >
       <Indicator>
-        <Tick />
+        {checked === undefined ? (
+          <Tick />
+        ) : checked === true ? (
+          <Tick />
+        ) : checked === 'indeterminate' ? (
+          <CheckIndeterminateSmall />
+        ) : null}
       </Indicator>
     </Root>
   );
---
+++ packages/ui-icons/src/CheckIndeterminateSmall.tsx
@@ -0,0 +1,40 @@
+import type { SVGProps } from 'react';
+const CheckIndeterminateSmall = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g id="prefix__check_indeterminate_small">
+      <mask
+        id="prefix__mask0_10532_14908"
+        style={{
+          maskType: 'alpha',
+        }}
+        maskUnits="userSpaceOnUse"
+        x={0}
+        y={0}
+        width={20}
+        height={20}
+      >
+        <rect
+          id="prefix__Bounding box"
+          width={20.0001}
+          height={20}
+          fill="currentColor"
+        />
+      </mask>
+      <g mask="url(#prefix__mask0_10532_14908)">
+        <path
+          id="prefix__check_indeterminate_small_2"
+          d="M5 10.8332V9.1665H15.0001V10.8332H5Z"
+          fill="#5A50FA"
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default CheckIndeterminateSmall;
