9a391a912d3a26776fa9bde400f193abe7aa75d7
Thomas Lathuiliere <thomas@checkmarble.com>
2024-01-03 17:36:05 +0100

    fix(types): issue with tsconfig

--- packages/app-builder/tailwind.config.ts
+++ packages/app-builder/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
 
-import sharedTailwindConfig from '../tailwind-preset/tailwind.config';
+import sharedTailwindConfig from '../tailwind-preset/src/tailwind.config';
 
 export default {
   presets: [sharedTailwindConfig],
