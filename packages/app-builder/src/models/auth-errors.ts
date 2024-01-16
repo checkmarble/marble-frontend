38cca9e9ece4c5a38d8e80fb9d1565ac5d74452e
Thomas Lathuiliere <thomas@checkmarble.com>
2024-01-16 16:11:45 +0100

    fix(auth): use ErrUnknownUser error code for no account

--- packages/app-builder/src/models/auth-errors.ts
+++ packages/app-builder/src/models/auth-errors.ts
@@ -9,15 +9,7 @@ export function adaptAuthErrors(error: unknown): AuthErrors {
     error instanceof HttpError &&
     error.status === 401 &&
     typeof error.data === 'string' &&
-    error.data?.includes('unknown user')
-  )
-    return 'NoAccount';
-
-  if (
-    error instanceof HttpError &&
-    error.status === 401 &&
-    typeof error.data === 'string' &&
-    error.data?.includes('not found')
+    error.data?.includes('ErrUnknownUser')
   )
     return 'NoAccount';
 
