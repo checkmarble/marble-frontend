5825ffc31a9a59fc5414bd8fd610cc40d10c416a
Thomas Lathuiliere <40292402+balzdur@users.noreply.github.com>
2024-01-23 12:27:00 +0100

    refactor(healthcheck): simplify healthcheck endpoint (#346)

--- packages/app-builder/src/routes/healthcheck.ts
+++ packages/app-builder/src/routes/healthcheck.ts
@@ -1,20 +1,5 @@
-import { type LoaderFunctionArgs } from '@remix-run/node';
-
-async function isServerLive(request: LoaderFunctionArgs['request']) {
-  const host =
-    request.headers.get('X-Forwarded-Host') ??
-    request.headers.get('host') ??
-    'NO_HOST';
-  const url = new URL('/', `http://${host}`);
-
-  return fetch(url.href, { method: 'HEAD' }).then((r) => {
-    if (!r.ok) return Promise.reject(r);
-  });
-}
-
-export async function loader({ request }: LoaderFunctionArgs) {
+export function loader() {
   try {
-    await Promise.all([isServerLive(request)]);
     return new Response('OK');
   } catch (error: unknown) {
     console.log('healthcheck ❌', { error });
