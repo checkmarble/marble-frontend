6e085aa645253ab702824fc879daf711d2f3b001
Thomas Lathuiliere <40292402+balzdur@users.noreply.github.com>
2024-04-10 16:20:13 +0200

    chore(deps): update oazapfts (#426)
    * chore(deps): update oazapfts
    
    * chore(deps): update oazapfts
    
    * chore(deps): remove unused dependencies

--- packages/app-builder/src/models/http-errors.ts
+++ packages/app-builder/src/models/http-errors.ts
@@ -5,32 +5,40 @@ import {
   NOT_FOUND,
   UNAUTHORIZED,
 } from '@app-builder/utils/http/http-status-codes';
-import { type HttpError } from 'oazapfts';
+import type * as Oazapfts from '@oazapfts/runtime';
 import * as z from 'zod';
 
-export function isHttpError(error: unknown): error is HttpError {
+export function isHttpError(error: unknown): error is Oazapfts.HttpError {
   return error instanceof Error && 'status' in error;
 }
 
-export function isStatusConflictHttpError(error: unknown): error is HttpError {
+export function isStatusConflictHttpError(
+  error: unknown,
+): error is Oazapfts.HttpError {
   return isHttpError(error) && error.status === CONFLICT;
 }
 
 export function isStatusBadRequestHttpError(
   error: unknown,
-): error is HttpError {
+): error is Oazapfts.HttpError {
   return isHttpError(error) && error.status === BAD_REQUEST;
 }
 
-export function isNotFoundHttpError(error: unknown): error is HttpError {
+export function isNotFoundHttpError(
+  error: unknown,
+): error is Oazapfts.HttpError {
   return isHttpError(error) && error.status === NOT_FOUND;
 }
 
-export function isUnauthorizedHttpError(error: unknown): error is HttpError {
+export function isUnauthorizedHttpError(
+  error: unknown,
+): error is Oazapfts.HttpError {
   return isHttpError(error) && error.status === UNAUTHORIZED;
 }
 
-export function isForbiddenHttpError(error: unknown): error is HttpError {
+export function isForbiddenHttpError(
+  error: unknown,
+): error is Oazapfts.HttpError {
   return isHttpError(error) && error.status === FORBIDDEN;
 }
 
@@ -38,8 +46,8 @@ const marbleErrorSchema = z.object({
   error_code: z.string().optional(),
   message: z.string(),
 });
-export function isMarbleError(error: HttpError): error is Omit<
-  HttpError,
+export function isMarbleError(error: Oazapfts.HttpError): error is Omit<
+  Oazapfts.HttpError,
   'data'
 > & {
   data: z.infer<typeof marbleErrorSchema>;
