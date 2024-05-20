11ca9d382b94dbe7254b1b959dfda3499e859b63
Thomas Lathuiliere <thomas@checkmarble.com>
2024-05-20 18:11:37 +0200

    feat(auth): add redirect to for logout

--- packages/app-builder/src/routes/ressources+/auth+/logout.tsx
+++ packages/app-builder/src/routes/ressources+/auth+/logout.tsx
@@ -1,13 +1,27 @@
 import { serverServices } from '@app-builder/services/init.server';
+import { getRoute } from '@app-builder/utils/routes';
 import {
   type ActionFunctionArgs,
   type LoaderFunctionArgs,
 } from '@remix-run/node';
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  await serverServices.authService.logout(request, { redirectTo: '/sign-in' });
+  const url = new URL(request.url);
+
+  const redirectToParam = url.searchParams.get('redirectTo');
+
+  let redirectTo = getRoute('/sign-in');
+  if (redirectToParam) {
+    redirectTo = `${redirectTo}?redirectTo=${redirectToParam}`;
+  }
+
+  await serverServices.authService.logout(request, {
+    redirectTo,
+  });
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  await serverServices.authService.logout(request, { redirectTo: '/sign-in' });
+  await serverServices.authService.logout(request, {
+    redirectTo: getRoute('/sign-in'),
+  });
 }
