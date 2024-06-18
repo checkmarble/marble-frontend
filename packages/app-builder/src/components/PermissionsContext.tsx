9412ede90bc7225ee928927fbef51b58204f6dfc
Thomas Lathuiliere <thomas@checkmarble.com>
2024-06-18 12:29:10 +0200

    refactor(PermissionsContext): remove unused exports

--- packages/app-builder/src/components/PermissionsContext.tsx
+++ packages/app-builder/src/components/PermissionsContext.tsx
@@ -1,7 +1,6 @@
 import { NewPermissions, type UserPermissions } from '@app-builder/models';
 import { createSimpleContext } from '@app-builder/utils/create-context';
-import { useNavigate } from '@remix-run/react';
-import { type PropsWithChildren, useEffect } from 'react';
+import { type PropsWithChildren } from 'react';
 
 const PermissionsContext = createSimpleContext<UserPermissions>(
   'PermissionsProvider',
@@ -19,19 +18,3 @@ export const PermissionsProvider = ({
 };
 
 export const usePermissionsContext = PermissionsContext.useValue;
-
-export const usePermissionRedirect = (
-  permission: keyof UserPermissions,
-  options: { redirectUrl: string },
-) => {
-  const userPermissions = usePermissionsContext();
-  const navigate = useNavigate();
-
-  const permissionValue = userPermissions[permission];
-
-  useEffect(() => {
-    if (!permissionValue) {
-      navigate(options.redirectUrl, { replace: true });
-    }
-  }, [navigate, options.redirectUrl, permissionValue]);
-};
