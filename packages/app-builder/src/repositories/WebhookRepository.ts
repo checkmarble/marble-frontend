2e9de0367d278e9fee373bf7abf17805c321d1a4
Thomas Lathuiliere <thomas@checkmarble.com>
2024-07-16 18:35:46 +0200

    feat(webhooks): add webhook models and repository

---
+++ packages/app-builder/src/repositories/WebhookRepository.ts
@@ -0,0 +1,51 @@
+import { type MarbleCoreApi } from '@app-builder/infra/marblecore-api';
+import {
+  adaptWebhook,
+  adaptWebhookRegisterBodyDto,
+  adaptWebhookUpdateBodyDto,
+  adaptWebhookWithSecret,
+  type Webhook,
+  type WebhookCreateBody,
+  type WebhookUpdateBody,
+  type WebhookWithSecret,
+} from '@app-builder/models/webhook';
+
+export interface WebhookRepository {
+  listWebhooks(): Promise<Webhook[]>;
+  createWebhook(args: {
+    webhookCreateBody: WebhookCreateBody;
+  }): Promise<WebhookWithSecret>;
+  updateWebhook(args: {
+    webhookId: string;
+    webhookUpdateBody: WebhookUpdateBody;
+  }): Promise<Webhook>;
+  deleteWebhook(args: { webhookId: string }): Promise<void>;
+}
+
+export function makeGetWebhookRepository() {
+  return (marbleCoreApiClient: MarbleCoreApi): WebhookRepository => ({
+    listWebhooks: async () => {
+      const { webhooks } = await marbleCoreApiClient.listWebhooks();
+
+      return webhooks.map(adaptWebhook);
+    },
+    createWebhook: async ({ webhookCreateBody }) => {
+      const { webhook } = await marbleCoreApiClient.createWebhook(
+        adaptWebhookRegisterBodyDto(webhookCreateBody),
+      );
+
+      return adaptWebhookWithSecret(webhook);
+    },
+    updateWebhook: async ({ webhookId, webhookUpdateBody }) => {
+      const { webhook } = await marbleCoreApiClient.updateWebhook(
+        webhookId,
+        adaptWebhookUpdateBodyDto(webhookUpdateBody),
+      );
+
+      return adaptWebhook(webhook);
+    },
+    deleteWebhook: async ({ webhookId }) => {
+      await marbleCoreApiClient.deleteWebhook(webhookId);
+    },
+  });
+}
