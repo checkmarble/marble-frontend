88bc6c30e4a3a598ae88995812e2b6ded0e0c569
Thomas Lathuiliere <thomas@checkmarble.com>
2024-08-02 17:57:07 +0200

    feat(license): add ruleSnoozing feature

--- packages/app-builder/src/repositories/LicenseRepository.ts
+++ packages/app-builder/src/repositories/LicenseRepository.ts
@@ -24,6 +24,7 @@ export function getLicenseRepository(
             dataEnrichment: true,
             userRoles: true,
             webhooks: true,
+            ruleSnoozing: true,
           },
         });
       }
