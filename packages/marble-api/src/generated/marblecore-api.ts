157116b1b1f770e807d1f4ac7695d9190152c4b5
Thomas Lathuiliere <thomas@checkmarble.com>
2024-09-06 11:09:36 +0200

    feat(api): add inbox filter to decision list

--- packages/marble-api/src/generated/marblecore-api.ts
+++ packages/marble-api/src/generated/marblecore-api.ts
@@ -696,13 +696,14 @@ export function getCredentials(opts?: Oazapfts.RequestOpts) {
 /**
  * List decisions
  */
-export function listDecisions({ caseId, endDate, hasCase, outcome, pivotValue, scenarioId, scheduledExecutionId, startDate, triggerObject, limit, next, offsetId, order, previous, sorting }: {
+export function listDecisions({ caseId, endDate, hasCase, outcome, pivotValue, scenarioId, inboxId, scheduledExecutionId, startDate, triggerObject, limit, next, offsetId, order, previous, sorting }: {
     caseId?: string[];
     endDate?: string;
     hasCase?: boolean;
     outcome?: Outcome[];
     pivotValue?: string;
     scenarioId?: string[];
+    inboxId?: string[];
     scheduledExecutionId?: string[];
     startDate?: string;
     triggerObject?: string[];
@@ -731,6 +732,7 @@ export function listDecisions({ caseId, endDate, hasCase, outcome, pivotValue, s
         "outcome[]": outcome,
         pivot_value: pivotValue,
         "scenario_id[]": scenarioId,
+        "inbox_id[]": inboxId,
         "scheduled_execution_id[]": scheduledExecutionId,
         start_date: startDate,
         "trigger_object[]": triggerObject,
