c003a7568cf5b8832177b4df762a08eeecbc5865
Thomas Lathuiliere <40292402+balzdur@users.noreply.github.com>
2024-09-09 11:47:22 +0200

    feat: add inbox filter to decision list (#534)
    * feat(api): add inbox filter to decision list
    
    * feat(decisions): add inbox filter to decision list

--- packages/app-builder/src/components/Decisions/Filters/FilterDetail/FilterDetail.tsx
+++ packages/app-builder/src/components/Decisions/Filters/FilterDetail/FilterDetail.tsx
@@ -3,6 +3,7 @@ import { assertNever } from 'typescript-utils';
 import { type DecisionFilterName } from '../filters';
 import { DecisionsDateRangeFilter } from './DecisionsDateRangeFilter';
 import { HasCaseFilter } from './HasCaseFilter';
+import { InboxFilter } from './InboxFilter';
 import { OutcomeFilter } from './OutcomeFilter';
 import { PivotValueFilter } from './PivotValueFilter';
 import { ScenarioFilter } from './ScenarioFilter';
@@ -18,6 +19,8 @@ export function FilterDetail({
       return <DecisionsDateRangeFilter />;
     case 'scenarioId':
       return <ScenarioFilter />;
+    case 'inboxId':
+      return <InboxFilter />;
     case 'outcome':
       return <OutcomeFilter />;
     case 'triggerObject':
---
+++ packages/app-builder/src/components/Decisions/Filters/FilterDetail/InboxFilter.tsx
@@ -0,0 +1,39 @@
+import { Highlight } from '@app-builder/components/Highlight';
+import { matchSorter } from '@app-builder/utils/search';
+import { useDeferredValue, useMemo, useState } from 'react';
+import { Input, SelectWithCombobox } from 'ui-design-system';
+
+import { useInboxFilter } from '../DecisionFiltersContext';
+
+export function InboxFilter() {
+  const [value, setSearchValue] = useState('');
+  const { inboxes, selectedInboxIds, setSelectedInboxIds } = useInboxFilter();
+  const searchValue = useDeferredValue(value);
+
+  const matches = useMemo(
+    () => matchSorter(inboxes, searchValue, { keys: ['name'] }),
+    [searchValue, inboxes],
+  );
+
+  return (
+    <div className="flex flex-col gap-2 p-2">
+      <SelectWithCombobox.Root
+        open
+        onSearchValueChange={setSearchValue}
+        selectedValue={selectedInboxIds}
+        onSelectedValueChange={setSelectedInboxIds}
+      >
+        <SelectWithCombobox.Combobox render={<Input />} autoSelect autoFocus />
+        <SelectWithCombobox.ComboboxList className="max-h-40">
+          {matches.map((inbox) => {
+            return (
+              <SelectWithCombobox.ComboboxItem key={inbox.id} value={inbox.id}>
+                <Highlight text={inbox.name} query={searchValue} />
+              </SelectWithCombobox.ComboboxItem>
+            );
+          })}
+        </SelectWithCombobox.ComboboxList>
+      </SelectWithCombobox.Root>
+    </div>
+  );
+}
