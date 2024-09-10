a23f37d6f4ba00021cb049f4050260c3da51f0dc
Thomas Lathuiliere <thomas@checkmarble.com>
2024-09-10 11:45:07 +0200

    feat(decision): new outcome input fields

--- packages/app-builder/src/routes/_builder+/scenarios+/$scenarioId+/i+/$iterationId+/_edit-view+/decision.tsx
+++ packages/app-builder/src/routes/_builder+/scenarios+/$scenarioId+/i+/$iterationId+/_edit-view+/decision.tsx
@@ -26,7 +26,7 @@ import { type ScenarioValidationErrorCodeDto } from 'marble-api';
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import * as R from 'remeda';
-import { Button, Collapsible } from 'ui-design-system';
+import { Button, Collapsible, input } from 'ui-design-system';
 import * as z from 'zod';
 
 import {
@@ -274,6 +274,58 @@ function ScoreThresholdsForm() {
           <EvaluationErrors errors={evaluationErrors} />
         )}
       </Form>
+      <div className="relative flex h-20 w-full flex-row">
+        <div className="bg-green-10 isolate flex h-10 flex-1 items-center justify-center rounded-l-md border-b-4 border-b-green-100">
+          <span className="text-s font-semibold text-green-100">
+            {t('decisions:outcome.approve')}
+          </span>
+        </div>
+
+        <div className="bg-grey-00 relative h-10 w-1">
+          <input
+            className={input({
+              className:
+                'absolute left-1/2 top-16 h-8 w-14 -translate-x-1/2 -translate-y-1/2 text-center',
+            })}
+          />
+        </div>
+
+        <div className="bg-yellow-10 flex h-10 flex-1 items-center justify-center border-b-4 border-b-yellow-100">
+          <span className="text-s font-semibold text-yellow-100">
+            {t('decisions:outcome.review')}
+          </span>
+        </div>
+
+        <div className="bg-grey-00 relative h-10 w-1">
+          <input
+            className={input({
+              className:
+                'absolute left-1/2 top-16 h-8 w-14 -translate-x-1/2 -translate-y-1/2 text-center',
+            })}
+          />
+        </div>
+
+        <div className="bg-orange-10 flex h-10 flex-1 items-center justify-center border-b-4 border-b-orange-100">
+          <span className="text-s font-semibold text-orange-100">
+            {t('decisions:outcome.review')}
+          </span>
+        </div>
+
+        <div className="bg-grey-00 relative h-10 w-1">
+          <input
+            className={input({
+              className:
+                'absolute left-1/2 top-16 h-8 w-14 -translate-x-1/2 -translate-y-1/2 text-center',
+            })}
+          />
+        </div>
+
+        <div className="bg-red-10 flex h-10 flex-1 items-center justify-center rounded-r-md border-b-4 border-b-red-100">
+          <span className="text-s font-semibold text-red-100">
+            {t('decisions:outcome.decline')}
+          </span>
+        </div>
+      </div>
     </FormProvider>
   );
 }
