d517329ee36ccb025d84d37f234782749fdc8a46
Thomas Lathuiliere <thomas@checkmarble.com>
2024-10-01 12:04:28 +0200

    feat(i18n): improve lng persistence

---
+++ packages/app-builder/src/repositories/SessionStorageRepositories/LngStorageRepository.ts
@@ -0,0 +1,27 @@
+import { createCookie, createCookieSessionStorage } from '@remix-run/node';
+
+import { type SessionStorageRepositoryOptions } from './SessionStorageRepository';
+
+export function getLngStorageRepository({
+  secrets,
+  secure,
+}: Omit<SessionStorageRepositoryOptions, 'maxAge'>) {
+  const lngCookie = createCookie('lng', {
+    sameSite: 'lax', // this helps with CSRF
+    path: '/', // remember to add this so the cookie will work in all routes
+    httpOnly: true,
+    secrets,
+    secure,
+  });
+
+  // export the whole sessionStorage object
+  const lngStorage = createCookieSessionStorage<{
+    lng: string;
+  }>({
+    cookie: lngCookie,
+  });
+
+  return { lngStorage };
+}
+
+export type LngStorageRepository = ReturnType<typeof getLngStorageRepository>;
