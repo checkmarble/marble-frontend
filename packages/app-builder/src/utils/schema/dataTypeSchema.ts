030d3f5e7ed33865d6b1aae7316d911e8b11234d
Thomas Lathuiliere <thomas@checkmarble.com>
2024-10-30 15:21:41 +0100

    feat: handle timestamp in constant

--- packages/app-builder/src/utils/schema/dataTypeSchema.ts
+++ packages/app-builder/src/utils/schema/dataTypeSchema.ts
@@ -1,36 +1,28 @@
 import * as z from 'zod';
 
 const protocolWhitelist = ['http:', 'https:'];
-const urlDataTypeSchema = z
-  .string()
-  .refine((value) => {
-    try {
-      const url = new URL(value);
-      return protocolWhitelist.includes(url.protocol);
-    } catch {
-      return false;
-    }
-  })
-  .transform((value) => ({
-    type: 'url' as const,
-    value,
-  }));
+export const urlDataTypeSchema = z.string().refine((value) => {
+  try {
+    const url = new URL(value);
+    return protocolWhitelist.includes(url.protocol);
+  } catch {
+    return false;
+  }
+});
 
-const dateTimeDataTypeSchema = z
-  .string()
-  .datetime({ offset: true })
-  .transform((value) => ({
-    type: 'datetime' as const,
-    value,
-  }));
-
-const numberDataTypeSchema = z.number().transform((value) => ({
-  type: 'number' as const,
-  value,
-}));
+export const dateTimeDataTypeSchema = z.string().datetime({ offset: true });
 
 export const knownDataTypeSchema = z.union([
-  urlDataTypeSchema,
-  dateTimeDataTypeSchema,
-  numberDataTypeSchema,
+  urlDataTypeSchema.transform((value) => ({
+    type: 'url' as const,
+    value,
+  })),
+  dateTimeDataTypeSchema.transform((value) => ({
+    type: 'datetime' as const,
+    value,
+  })),
+  z.number().transform((value) => ({
+    type: 'number' as const,
+    value,
+  })),
 ]);
