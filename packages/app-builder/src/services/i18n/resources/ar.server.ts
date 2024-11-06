bb46ace61e7f694c3740b166f2670c74fef715f2
Ibrahim Morghim <morghim@outlook.sa>
2024-11-06 16:51:33 +0300

    feat(i18n): add arabic translation (#578)
    feat: add arabic language

---
+++ packages/app-builder/src/services/i18n/resources/ar.server.ts
@@ -0,0 +1,33 @@
+import api from '@app-builder/locales/ar/api.json';
+import auth from '@app-builder/locales/ar/auth.json';
+import cases from '@app-builder/locales/ar/cases.json';
+import common from '@app-builder/locales/ar/common.json';
+import data from '@app-builder/locales/ar/data.json';
+import decisions from '@app-builder/locales/ar/decisions.json';
+import filters from '@app-builder/locales/ar/filters.json';
+import lists from '@app-builder/locales/ar/lists.json';
+import navigation from '@app-builder/locales/ar/navigation.json';
+import scenarios from '@app-builder/locales/ar/scenarios.json';
+import scheduledExecution from '@app-builder/locales/ar/scheduledExecution.json';
+import settings from '@app-builder/locales/ar/settings.json';
+import upload from '@app-builder/locales/ar/upload.json';
+import workflows from '@app-builder/locales/ar/workflows.json';
+import transfercheck from '@app-builder/locales/ar/transfercheck.json';
+
+export const ar = {
+  api,
+  cases,
+  common,
+  data,
+  decisions,
+  filters,
+  navigation,
+  lists,
+  auth,
+  scenarios,
+  scheduledExecution,
+  settings,
+  transfercheck,
+  upload,
+  workflows,
+};
--- packages/app-builder/src/services/i18n/resources/resources.server.ts
+++ packages/app-builder/src/services/i18n/resources/resources.server.ts
@@ -1,8 +1,10 @@
 import { type supportedLngs } from '../i18n-config';
 import { en } from './en.server';
 import { fr } from './fr.server';
+import { ar } from './ar.server';
 
 export const resources = {
   en,
   fr,
+  ar,
 } satisfies Record<(typeof supportedLngs)[number], typeof en>;
