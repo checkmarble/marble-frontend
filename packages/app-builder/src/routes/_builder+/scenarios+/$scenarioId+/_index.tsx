51f48db00116208a707e52613c395c3715cdd1cb
Thomas Lathuiliere <40292402+balzdur@users.noreply.github.com>
2024-11-15 16:17:36 +0100

    feat: add home page with version picker (#594)

--- packages/app-builder/src/routes/_builder+/scenarios+/$scenarioId+/_index.tsx
+++ packages/app-builder/src/routes/_builder+/scenarios+/$scenarioId+/_index.tsx
@@ -1,62 +1,25 @@
-import { setToastMessage } from '@app-builder/components/MarbleToaster';
 import { serverServices } from '@app-builder/services/init.server';
 import { getRoute } from '@app-builder/utils/routes';
-import { fromParams, fromUUID } from '@app-builder/utils/short-uuid';
-import { type LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { type LoaderFunctionArgs } from '@remix-run/node';
 import { type Namespace } from 'i18next';
-import * as R from 'remeda';
 
 export const handle = {
   i18n: ['scenarios'] satisfies Namespace,
 };
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
-  const {
-    authService,
-    toastSessionService: { getSession, commitSession },
-  } = serverServices;
-  const { scenario } = await authService.isAuthenticated(request, {
+  const { authService } = serverServices;
+  const { scenarioId } = params;
+  if (!scenarioId) {
+    return {
+      redirect: getRoute('/scenarios/'),
+    };
+  }
+
+  return authService.isAuthenticated(request, {
+    successRedirect: getRoute('/scenarios/:scenarioId/home', {
+      scenarioId,
+    }),
     failureRedirect: getRoute('/sign-in'),
   });
-
-  const scenarioId = fromParams(params, 'scenarioId');
-
-  const currentScenario = await scenario.getScenario({ scenarioId });
-
-  if (currentScenario.liveVersionId) {
-    return redirect(
-      getRoute('/scenarios/:scenarioId/i/:iterationId', {
-        scenarioId: fromUUID(scenarioId),
-        iterationId: fromUUID(currentScenario.liveVersionId),
-      }),
-    );
-  }
-
-  const scenarioIterations = await scenario.listScenarioIterations({
-    scenarioId,
-  });
-
-  const lastScenarioIteration = R.pipe(
-    scenarioIterations,
-    R.firstBy([({ createdAt }) => createdAt, 'desc']),
-  );
-
-  if (!lastScenarioIteration) {
-    const session = await getSession(request);
-    setToastMessage(session, {
-      type: 'error',
-      messageKey: 'common:empty_scenario_iteration_list',
-    });
-
-    return redirect(getRoute('/scenarios/'), {
-      headers: { 'Set-Cookie': await commitSession(session) },
-    });
-  }
-
-  return redirect(
-    getRoute('/scenarios/:scenarioId/i/:iterationId', {
-      scenarioId: fromUUID(scenarioId),
-      iterationId: fromUUID(lastScenarioIteration.id),
-    }),
-  );
 }
