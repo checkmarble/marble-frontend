af16b50504b58f81e73237200332b54a4e6b60bf
Pascal Delange <pascal@checkmarble.com>
2025-01-02 09:31:56 +0100

    properly encode file names for csv download
    fix

--- packages/app-builder/src/utils/download-file.ts
+++ packages/app-builder/src/utils/download-file.ts
@@ -5,12 +5,22 @@ const TIME_TO_OPEN_DOWNLOAD_MODALE = 150;
 /**
  * Utility function to download a file in the browser
  */
-export async function downloadFile(url: string, filename?: string) {
+export async function downloadFile(url: string, filename: string) {
   return new Promise<void>((resolve, reject) => {
     try {
       const a = document.createElement('a');
       a.href = url;
-      a.download = filename || 'download';
+      a.download = filename
+        .replace(/'/g, '%27')
+        .replace(/\(/g, '%28')
+        .replace(/\)/g, '%29')
+        .replace(/\*/g, '%2A')
+        // These characters can be decoded safely
+        .replace(/%20/g, ' ')
+        .replace(/%2C/g, ',')
+        .replace(/%7C/g, '|')
+        .replace(/%60/g, '`')
+        .replace(/%5E/g, '^');
 
       // Click handler that releases the object URL after the element has been clicked
       // This is required for one-off downloads of the blob content
