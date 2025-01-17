86e4db6aad9d3ee66d45928630642cc324f52cc3
Kevin Abatan <abatan.k@gmail.com>
2025-01-17 18:36:36 +0100

    feat: connect search input to filters

--- packages/app-builder/src/components/Cases/Filters/filters.ts
+++ packages/app-builder/src/components/Cases/Filters/filters.ts
@@ -2,7 +2,7 @@ import { type ParseKeys } from 'i18next';
 import { match } from 'ts-pattern';
 import { type IconName } from 'ui-icons';
 
-export const casesFilterNames = ['dateRange', 'statuses', 'name'] as const;
+export const casesFilterNames = ['dateRange', 'statuses'] as const;
 
 export type CasesFilterName = (typeof casesFilterNames)[number];
 
@@ -10,12 +10,10 @@ export const getFilterIcon = (filterName: CasesFilterName) =>
   match<CasesFilterName, IconName>(filterName)
     .with('dateRange', () => 'calendar-month')
     .with('statuses', () => 'category')
-    .with('name', () => 'search')
     .exhaustive();
 
 export const getFilterTKey = (filterName: CasesFilterName) =>
   match<CasesFilterName, ParseKeys<['cases']>>(filterName)
     .with('dateRange', () => 'cases:case.date')
     .with('statuses', () => 'cases:case.status')
-    .with('name', () => 'cases:case.name')
     .exhaustive();
