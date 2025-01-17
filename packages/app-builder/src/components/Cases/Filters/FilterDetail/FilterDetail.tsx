912ef9d2bc8cbb0a6f80087ac87fba24acc69e98
Kevin Abatan <abatan.k@gmail.com>
2025-01-17 18:44:52 +0100

    fix: last typing error

--- packages/app-builder/src/components/Cases/Filters/FilterDetail/FilterDetail.tsx
+++ packages/app-builder/src/components/Cases/Filters/FilterDetail/FilterDetail.tsx
@@ -2,12 +2,10 @@ import { match } from 'ts-pattern';
 
 import { type CasesFilterName } from '../filters';
 import { CasesDateRangeFilter } from './CasesDateRangeFilter';
-import { NameFilter } from './NameFilter';
 import { StatusesFilter } from './StatusesFilter';
 
 export const FilterDetail = ({ filterName }: { filterName: CasesFilterName }) =>
   match(filterName)
     .with('dateRange', () => <CasesDateRangeFilter />)
     .with('statuses', () => <StatusesFilter />)
-    .with('name', () => <NameFilter />)
     .exhaustive();
