82b9d8d57f44e93f8c56714910ddfaa79fc5b7f5
Kevin Abatan <abatan.k@gmail.com>
2025-03-04 12:54:13 +0100

    feat: updating create inboxes form

--- packages/app-builder/src/routes/ressources+/settings+/inboxes+/create.tsx
+++ packages/app-builder/src/routes/ressources+/settings+/inboxes+/create.tsx
@@ -1,19 +1,17 @@
-import { FormErrorOrDescription } from '@app-builder/components/Form/FormErrorOrDescription';
-import { FormField } from '@app-builder/components/Form/FormField';
-import { FormInput } from '@app-builder/components/Form/FormInput';
-import { FormLabel } from '@app-builder/components/Form/FormLabel';
+import { FormErrorOrDescription } from '@app-builder/components/Form/Tanstack/FormErrorOrDescription';
+import { FormInput } from '@app-builder/components/Form/Tanstack/FormInput';
+import { FormLabel } from '@app-builder/components/Form/Tanstack/FormLabel';
 import { setToastMessage } from '@app-builder/components/MarbleToaster';
 import { serverServices } from '@app-builder/services/init.server';
 import { getRoute } from '@app-builder/utils/routes';
 import { fromUUID } from '@app-builder/utils/short-uuid';
-import { FormProvider, getFormProps, getInputProps, useForm } from '@conform-to/react';
-import { getZodConstraint, parseWithZod } from '@conform-to/zod';
 import { type ActionFunctionArgs, json, redirect } from '@remix-run/node';
 import { useFetcher, useNavigation } from '@remix-run/react';
+import { useForm } from '@tanstack/react-form';
 import { type Namespace } from 'i18next';
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, ModalV2 } from 'ui-design-system';
+import { Button, HiddenInputs, ModalV2 } from 'ui-design-system';
 import { Icon } from 'ui-icons';
 import { z } from 'zod';
 
@@ -25,10 +23,13 @@ export const redirectRouteOptions = [
   '/cases/inboxes/:inboxId',
   '/settings/inboxes/:inboxId',
 ] as const;
+
 const createInboxFormSchema = z.object({
   name: z.string().min(1),
   redirectRoute: z.enum(redirectRouteOptions).optional(),
 });
+
+type CreateInboxForm = z.infer<typeof createInboxFormSchema>;
 
 export async function action({ request }: ActionFunctionArgs) {
   const {
@@ -36,42 +37,54 @@ export async function action({ request }: ActionFunctionArgs) {
     i18nextService: { getFixedT },
     toastSessionService: { getSession, commitSession },
   } = serverServices;
-  const { inbox } = await authService.isAuthenticated(request, {
-    failureRedirect: getRoute('/sign-in'),
-  });
 
-  const formData = await request.formData();
-  const submission = parseWithZod(formData, { schema: createInboxFormSchema });
+  const [{ inbox }, data, session, t] = await Promise.all([
+    authService.isAuthenticated(request, {
+      failureRedirect: getRoute('/sign-in'),
+    }),
+    request.json(),
+    getSession(request),
+    getFixedT(request, ['common']),
+  ]);
 
-  if (submission.status !== 'success') {
-    return json(submission.reply());
+  const result = createInboxFormSchema.safeParse(data);
+
+  if (!result.success) {
+    return json(
+      { status: 'error', errors: result.error.flatten() },
+      { headers: { 'Set-Cookie': await commitSession(session) } },
+    );
   }
 
   try {
-    const createdInbox = await inbox.createInbox({
-      name: submission.value.name,
-    });
-    if (submission.value.redirectRoute)
+    const createdInbox = await inbox.createInbox(data);
+
+    if (data.redirectRoute)
       return redirect(
-        getRoute(submission.value.redirectRoute, {
+        getRoute(data.redirectRoute, {
           inboxId: fromUUID(createdInbox.id),
         }),
       );
-    return json(submission.reply());
-  } catch (error) {
-    const session = await getSession(request);
-    const t = await getFixedT(request, ['common']);
-
-    const formError = t('common:errors.unknown');
 
     setToastMessage(session, {
-      type: 'error',
-      message: formError,
+      type: 'success',
+      messageKey: 'common:success.save',
     });
 
-    return json(submission.reply({ formErrors: [formError] }), {
-      headers: { 'Set-Cookie': await commitSession(session) },
+    return json(
+      { status: 'success', errors: [] },
+      { headers: { 'Set-Cookie': await commitSession(session) } },
+    );
+  } catch (error) {
+    setToastMessage(session, {
+      type: 'error',
+      message: t('common:errors.unknown'),
     });
+
+    return json(
+      { status: 'error', errors: [] },
+      { headers: { 'Set-Cookie': await commitSession(session) } },
+    );
   }
 }
 
@@ -81,10 +94,10 @@ export function CreateInbox({
   redirectRoutePath?: (typeof redirectRouteOptions)[number];
 }) {
   const { t } = useTranslation(handle.i18n);
-  const [open, setOpen] = React.useState(false);
-
+  const [open, setOpen] = useState(false);
   const navigation = useNavigation();
-  React.useEffect(() => {
+
+  useEffect(() => {
     if (navigation.state === 'loading') {
       setOpen(false);
     }
@@ -114,55 +127,64 @@ export function CreateInboxContent({
   setOpen: (open: boolean) => void;
 }) {
   const { t } = useTranslation(handle.i18n);
-
   const fetcher = useFetcher<typeof action>();
-  React.useEffect(() => {
+
+  useEffect(() => {
     if (fetcher?.data?.status === 'success') {
       setOpen(false);
     }
   }, [setOpen, fetcher?.data?.status]);
 
-  const [form, fields] = useForm({
-    shouldRevalidate: 'onInput',
-    defaultValue: { name: '', redirectRoute: redirectRoutePath },
-    lastResult: fetcher.data,
-    constraint: getZodConstraint(createInboxFormSchema),
-    onValidate({ formData }) {
-      return parseWithZod(formData, {
-        schema: createInboxFormSchema,
-      });
+  const form = useForm<CreateInboxForm>({
+    defaultValues: { name: '', redirectRoute: redirectRoutePath },
+    validators: {
+      onChangeAsync: createInboxFormSchema,
+      onBlurAsync: createInboxFormSchema,
+      onSubmitAsync: createInboxFormSchema,
+    },
+    onSubmit: ({ value, formApi }) => {
+      if (formApi.state.isValid) {
+        fetcher.submit(value, { method: 'POST', encType: 'application/json' });
+      }
     },
   });
 
   return (
-    <FormProvider context={form.context}>
-      <fetcher.Form
-        method="post"
-        action={getRoute('/ressources/settings/inboxes/create')}
-        {...getFormProps(form)}
-      >
-        <ModalV2.Title>{t('settings:inboxes.new_inbox.explain')}</ModalV2.Title>
-        <div className="flex flex-col gap-6 p-6">
-          <input
-            {...getInputProps(fields.redirectRoute, { type: 'hidden' })}
-            key={fields.redirectRoute.key}
-          />
-          <FormField name={fields.name.name} className="group flex flex-col gap-2">
-            <FormLabel>{t('settings:inboxes.new_inbox.name')}</FormLabel>
-            <FormInput type="text" />
-            <FormErrorOrDescription />
-          </FormField>
-          <div className="flex flex-1 flex-row gap-2">
-            <ModalV2.Close render={<Button className="flex-1" variant="secondary" />}>
-              {t('common:cancel')}
-            </ModalV2.Close>
-            <Button className="flex-1" variant="primary" type="submit" name="create">
-              <Icon icon="new-inbox" className="size-5" />
-              {t('settings:inboxes.new_inbox.create')}
-            </Button>
-          </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        form.handleSubmit();
+      }}
+    >
+      <ModalV2.Title>{t('settings:inboxes.new_inbox.explain')}</ModalV2.Title>
+      <div className="flex flex-col gap-6 p-6">
+        <HiddenInputs redirectRoute={redirectRoutePath} />
+        <form.Field name="name">
+          {(field) => (
+            <div className="group flex flex-col gap-2">
+              <FormLabel name={field.name}>{t('settings:inboxes.new_inbox.name')}</FormLabel>
+              <FormInput
+                name={field.name}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.currentTarget.value)}
+                valid={field.state.meta.errors.length === 0}
+                type="text"
+              />
+              <FormErrorOrDescription errors={field.state.meta.errors} />
+            </div>
+          )}
+        </form.Field>
+        <div className="flex flex-1 flex-row gap-2">
+          <ModalV2.Close render={<Button className="flex-1" variant="secondary" />}>
+            {t('common:cancel')}
+          </ModalV2.Close>
+          <Button className="flex-1" variant="primary" type="submit" name="create">
+            <Icon icon="new-inbox" className="size-5" />
+            {t('settings:inboxes.new_inbox.create')}
+          </Button>
         </div>
-      </fetcher.Form>
-    </FormProvider>
+      </div>
+    </form>
   );
 }
