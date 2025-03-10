ccd5d05ed57595f4395ac424dcc5b08effaa1f77
Kevin Abatan <abatan.k@gmail.com>
2025-03-10 15:50:43 +0100

    refactor: update create value to list

--- packages/app-builder/src/routes/ressources+/lists+/value_create.tsx
+++ packages/app-builder/src/routes/ressources+/lists+/value_create.tsx
@@ -1,21 +1,14 @@
-import {
-  FormControl,
-  FormError,
-  FormField,
-  FormItem,
-  FormLabel,
-} from '@app-builder/components/Form';
+import { FormErrorOrDescription } from '@app-builder/components/Form/Tanstack/FormErrorOrDescription';
+import { FormInput } from '@app-builder/components/Form/Tanstack/FormInput';
+import { FormLabel } from '@app-builder/components/Form/Tanstack/FormLabel';
 import { serverServices } from '@app-builder/services/init.server';
-import { parseFormSafe } from '@app-builder/utils/input-validation';
 import { getRoute } from '@app-builder/utils/routes';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { type ActionFunctionArgs, json } from '@remix-run/node';
 import { useFetcher } from '@remix-run/react';
+import { useForm } from '@tanstack/react-form';
 import { type Namespace } from 'i18next';
-import { useEffect, useState } from 'react';
-import { Form, FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { Button, Input, Modal } from 'ui-design-system';
+import { Button, ModalV2 as Modal } from 'ui-design-system';
 import { Icon } from 'ui-icons';
 import { z } from 'zod';
 
@@ -28,106 +21,99 @@ const addValueFormSchema = z.object({
   value: z.string().nonempty(),
 });
 
+type AddValueForm = z.infer<typeof addValueFormSchema>;
+
 export async function action({ request }: ActionFunctionArgs) {
   const { authService } = serverServices;
-  const { customListsRepository } = await authService.isAuthenticated(request, {
-    failureRedirect: getRoute('/sign-in'),
-  });
-  const parsedForm = await parseFormSafe(request, addValueFormSchema);
-  if (!parsedForm.success) {
-    parsedForm.error.flatten((issue) => issue);
 
-    return json({
-      success: false as const,
-      values: parsedForm.formData,
-      error: parsedForm.error.format(),
-    });
-  }
-  const { listId, value } = parsedForm.data;
-  await customListsRepository.createCustomListValue(listId, {
-    value: value,
+  const [raw, { customListsRepository }] = await Promise.all([
+    request.json(),
+    authService.isAuthenticated(request, {
+      failureRedirect: getRoute('/sign-in'),
+    }),
+  ]);
+
+  const { success, error, data } = addValueFormSchema.safeParse(raw);
+
+  if (!success) return json({ success: false as const, errors: error.format() });
+
+  await customListsRepository.createCustomListValue(data.listId, {
+    value: data.value,
   });
-  return json({
-    success: true as const,
-    values: parsedForm.data,
-    error: null,
-  });
+
+  return json({ success: true as const });
 }
 
 export function NewListValue({ listId }: { listId: string }) {
   const { t } = useTranslation(handle.i18n);
-  const fetcher = useFetcher<typeof action>();
+  const { submit } = useFetcher<typeof action>();
 
-  const formMethods = useForm<z.infer<typeof addValueFormSchema>>({
-    progressive: true,
-    resolver: zodResolver(addValueFormSchema),
+  const form = useForm<AddValueForm>({
     defaultValues: {
       listId,
       value: '',
     },
+    onSubmit: ({ value, formApi }) => {
+      if (formApi.state.isValid) {
+        submit(value, {
+          method: 'POST',
+          action: getRoute('/ressources/lists/value_create'),
+          encType: 'application/json',
+        });
+      }
+    },
+    validators: {
+      onChangeAsync: addValueFormSchema,
+      onBlurAsync: addValueFormSchema,
+      onSubmitAsync: addValueFormSchema,
+    },
   });
-  const { control, register } = formMethods;
-
-  const [isOpen, setIsOpen] = useState(false);
-  useEffect(() => {
-    if (fetcher.state === 'idle' && fetcher.data?.success) {
-      setIsOpen(false);
-      formMethods.reset();
-    }
-  }, [fetcher.data?.success, fetcher.state, formMethods]);
 
   return (
-    <Modal.Root open={isOpen} onOpenChange={setIsOpen}>
-      <Modal.Trigger asChild>
-        <Button>
-          <Icon icon="plus" className="size-6" />
-          {t('lists:create_value.title')}
-        </Button>
+    <Modal.Root>
+      <Modal.Trigger render={<Button />}>
+        <Icon icon="plus" className="size-6" />
+        {t('lists:create_value.title')}
       </Modal.Trigger>
       <Modal.Content>
-        <Form
-          control={control}
-          onSubmit={({ formData }) => {
-            fetcher.submit(formData, {
-              method: 'POST',
-              action: getRoute('/ressources/lists/value_create'),
-            });
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
           }}
         >
-          <FormProvider {...formMethods}>
-            <input hidden {...register('listId')} />
-            <Modal.Title>{t('lists:create_value.title')}</Modal.Title>
-            <div className="flex flex-col gap-6 p-6">
-              <FormField
-                name="value"
-                control={control}
-                render={({ field }) => (
-                  <FormItem className="flex flex-col gap-2">
-                    <FormLabel>{t('lists:value', { count: 1 })}</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="text"
-                        placeholder={t('lists:create_value.value_placeholder')}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormError />
-                  </FormItem>
-                )}
-              />
-              <div className="flex flex-1 flex-row gap-2">
-                <Modal.Close asChild>
-                  <Button className="flex-1" variant="secondary">
-                    {t('common:cancel')}
-                  </Button>
-                </Modal.Close>
-                <Button className="flex-1" variant="primary" type="submit" name="create">
-                  {t('common:save')}
-                </Button>
-              </div>
+          <Modal.Title>{t('lists:create_value.title')}</Modal.Title>
+          <div className="flex flex-col gap-6 p-6">
+            <form.Field name="value">
+              {(field) => (
+                <div className="flex flex-col gap-2">
+                  <FormLabel name={field.name}>{t('lists:value', { count: 1 })}</FormLabel>
+                  <FormInput
+                    type="text"
+                    name={field.name}
+                    defaultValue={field.state.value}
+                    onChange={(e) => field.handleChange(e.currentTarget.value)}
+                    onBlur={field.handleBlur}
+                    valid={field.state.meta.errors.length === 0}
+                    placeholder={t('lists:create_value.value_placeholder')}
+                  />
+                  <FormErrorOrDescription errors={field.state.meta.errors} />
+                </div>
+              )}
+            </form.Field>
+            <div className="flex flex-1 flex-row gap-2">
+              <Modal.Close render={<Button className="flex-1" type="button" variant="secondary" />}>
+                {t('common:cancel')}
+              </Modal.Close>
+              <Modal.Close
+                render={<Button className="flex-1" variant="primary" type="submit" name="create" />}
+              >
+                {t('common:save')}
+              </Modal.Close>
             </div>
-          </FormProvider>
-        </Form>
+          </div>
+        </form>
       </Modal.Content>
     </Modal.Root>
   );
