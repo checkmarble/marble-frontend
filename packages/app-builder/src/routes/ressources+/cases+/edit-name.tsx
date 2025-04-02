547276c1a546485509d55e23637f8d94ebd66caa
Kevin Abatan <abatan.k@gmail.com>
2025-04-02 17:15:18 +0200

    feat: adding auto width input to the mix

--- packages/app-builder/src/routes/ressources+/cases+/edit-name.tsx
+++ packages/app-builder/src/routes/ressources+/cases+/edit-name.tsx
@@ -1,4 +1,5 @@
 import { casesI18n } from '@app-builder/components/Cases/cases-i18n';
+import AutoWidthInput from '@app-builder/components/Form/AutoWidthInput';
 import { FormErrorOrDescription } from '@app-builder/components/Form/Tanstack/FormErrorOrDescription';
 import { initServerServices } from '@app-builder/services/init.server';
 import { getFieldErrors, handleSubmit } from '@app-builder/utils/form';
@@ -71,7 +72,7 @@ export const EditCaseName = ({ name, id }: { name: string; id: string }) => {
         {(field) => (
           <div className="flex w-full flex-col gap-1">
             <div className="flex items-center gap-2">
-              <input
+              <AutoWidthInput
                 type="text"
                 name={field.name}
                 disabled={!isEditing}
@@ -79,6 +80,9 @@ export const EditCaseName = ({ name, id }: { name: string; id: string }) => {
                 onChange={(e) => field.handleChange(e.currentTarget.value)}
                 onBlur={field.handleBlur}
                 className="text-grey-00 text-l w-full border-none bg-transparent font-normal outline-none"
+                extraWidth={10}
+                maxWidth={750}
+                placeholderIsMinWidth
                 placeholder={t('cases:case.name')}
               />
               {!isEditing ? (
