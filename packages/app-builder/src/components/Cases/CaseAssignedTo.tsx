28566596c96eeabb4fd34bb9d51fa72bc89736db
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-04-24 17:54:43 +0200

    refactor: clean up unnecessary CSS classes

--- packages/app-builder/src/components/Cases/CaseAssignedTo.tsx
+++ packages/app-builder/src/components/Cases/CaseAssignedTo.tsx
@@ -14,31 +14,27 @@ export function CaseAssignedTo({ userId }: { userId: string }) {
   return (
     <Tooltip.Default
       content={
-        <div className="flex flex-col gap-1">
-          <div key={user?.userId ?? 0} className="flex flex-row items-center gap-1">
-            <Avatar
-              key={user?.userId}
-              size="xs"
-              firstName={user?.firstName}
-              lastName={user?.lastName}
-            />
-            <div className="text-grey-00 text-xs font-normal capitalize">
-              {getFullName(user) || t('cases:case_detail.unknown_user')}
-            </div>
+        <div key={user?.userId ?? 0} className="flex flex-row items-center gap-1">
+          <Avatar
+            key={user?.userId}
+            size="xs"
+            firstName={user?.firstName}
+            lastName={user?.lastName}
+          />
+          <div className="text-grey-00 text-xs font-normal capitalize">
+            {getFullName(user) || t('cases:case_detail.unknown_user')}
           </div>
         </div>
       }
     >
       <div className="flex w-fit flex-row items-center gap-1">
-        <div className="isolate flex -space-x-4 overflow-hidden">
-          <Avatar
-            key={user?.userId ?? 0}
-            className="border-grey-100 border-2"
-            size="s"
-            firstName={user?.firstName}
-            lastName={user?.lastName}
-          />
-        </div>
+        <Avatar
+          key={user?.userId ?? 0}
+          className="border-grey-100 border-2"
+          size="s"
+          firstName={user?.firstName}
+          lastName={user?.lastName}
+        />
       </div>
     </Tooltip.Default>
   );
