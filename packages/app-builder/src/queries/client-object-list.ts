fb1518b066f891464213e51725f65489ba3e91f3
Christopher Debove <christopher@checkmarble.com>
2025-05-06 16:43:55 +0200

    fix(case manager): use infinite loading in data explorer table pagination

--- packages/app-builder/src/queries/client-object-list.ts
+++ packages/app-builder/src/queries/client-object-list.ts
@@ -1,6 +1,6 @@
 import { type DataTableObjectListResource } from '@app-builder/routes/ressources+/data+/$tableName.list-objects';
 import { getRoute } from '@app-builder/utils/routes';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import QueryString from 'qs';
 
 const endpoint = (tableName: string, qs: string) =>
@@ -16,23 +16,25 @@ type UseClientObjectListQueryParams = {
     filterFieldValue: string | number;
     orderingFieldName: string;
     limit?: number;
-    offsetId: string | number | null;
   };
 };
 export function useClientObjectListQuery(params: UseClientObjectListQueryParams) {
-  const strQs = QueryString.stringify(
-    { ...params.params },
-    { skipNulls: true, addQueryPrefix: true },
-  );
+  const strQs = QueryString.stringify(params.params, { skipNulls: true, addQueryPrefix: true });
   const queryKey = ['resources', 'data-list-object', params.tableName!, strQs] as const;
 
-  return useQuery({
+  return useInfiniteQuery({
     queryKey,
-    queryFn: async ({ queryKey: [_r, __, tableName, qs] }) => {
-      const response = await fetch(endpoint(tableName, qs), {
+    queryFn: async ({ queryKey: [_r, __, tableName, qs], pageParam }) => {
+      const qsWithPageParam = pageParam ? `${qs}&offsetId=${pageParam}` : qs;
+      const response = await fetch(endpoint(tableName, qsWithPageParam), {
         method: 'GET',
       });
+
       return response.json() as Promise<DataTableObjectListResource>;
+    },
+    initialPageParam: null as string | number | null,
+    getNextPageParam: (lastPage, _pages) => {
+      return lastPage.clientDataListResponse.pagination.nextCursorId;
     },
     enabled: !!params.tableName,
   });
