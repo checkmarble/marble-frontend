f76212e00dcf8375d419bf5d047612a1a107cb3a
Kevin Abatan <abatan.k@gmail.com>
2025-05-07 16:41:51 +0200

    feat: add rule snooze event + decision reviewed event

--- packages/app-builder/src/components/Cases/Events/DecisionReviewed.tsx
+++ packages/app-builder/src/components/Cases/Events/DecisionReviewed.tsx
@@ -1,5 +1,39 @@
+import { OutcomeBadge } from '@app-builder/components/Decisions';
 import { type DecisionReviewedEvent } from '@app-builder/models/cases';
+import { useOrganizationUsers } from '@app-builder/services/organization/organization-users';
+import { getFullName } from '@app-builder/services/user';
+import { useMemo } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Icon } from 'ui-icons';
 
-export const DecisionReviewedDetail = (_: { event: DecisionReviewedEvent }) => {
-  return <span>Decision reviewed</span>;
+import { casesI18n } from '../cases-i18n';
+import { EventTime } from './Time';
+
+export const DecisionReviewedDetail = ({ event }: { event: DecisionReviewedEvent }) => {
+  const { t } = useTranslation(casesI18n);
+  const { getOrgUserById } = useOrganizationUsers();
+  const user = useMemo(
+    () => (event.userId ? getOrgUserById(event.userId) : undefined),
+    [event.userId, getOrgUserById],
+  );
+
+  return (
+    <div key={event.id} className="flex w-full items-center gap-2">
+      <div className="bg-grey-100 border-grey-90 flex size-6 shrink-0 grow-0 items-center justify-center rounded-full border">
+        <Icon icon="manage-search" className="text-grey-00 size-3" />
+      </div>
+      <span className="text-grey-00 inline-flex h-full items-center whitespace-pre text-xs">
+        <Trans
+          t={t}
+          i18nKey="cases:case_detail.history.event_detail.decision_reviewed"
+          components={{
+            Actor: <span className="font-bold capitalize" />,
+            Status: <OutcomeBadge outcome={event.status} />,
+          }}
+          values={{ actor: user ? getFullName(user) : 'Workflow' }}
+        />
+      </span>
+      <EventTime time={event.createdAt} />
+    </div>
+  );
 };
--- packages/app-builder/src/components/Cases/Events/RuleSnoozed.tsx
+++ packages/app-builder/src/components/Cases/Events/RuleSnoozed.tsx
@@ -1,5 +1,37 @@
 import { type RuleSnoozeCreatedEvent } from '@app-builder/models/cases';
+import { useOrganizationUsers } from '@app-builder/services/organization/organization-users';
+import { getFullName } from '@app-builder/services/user';
+import { useMemo } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Icon } from 'ui-icons';
 
-export const RuleSnoozeCreatedDetail = (_: { event: RuleSnoozeCreatedEvent }) => {
-  return <span>Rule snooze created</span>;
+import { casesI18n } from '../cases-i18n';
+import { EventTime } from './Time';
+
+export const RuleSnoozeCreatedDetail = ({ event }: { event: RuleSnoozeCreatedEvent }) => {
+  const { t } = useTranslation(casesI18n);
+  const { getOrgUserById } = useOrganizationUsers();
+  const user = useMemo(
+    () => (event.userId ? getOrgUserById(event.userId) : undefined),
+    [event.userId, getOrgUserById],
+  );
+
+  return (
+    <div key={event.id} className="flex w-full items-center gap-2">
+      <div className="bg-grey-100 border-grey-90 flex size-6 shrink-0 grow-0 items-center justify-center rounded-full border">
+        <Icon icon="snooze" className="text-grey-00 size-3" />
+      </div>
+      <span className="text-grey-00 inline-flex h-full items-center whitespace-pre text-xs">
+        <Trans
+          t={t}
+          i18nKey="cases:case_detail.history.event_detail.rule_snooze_created"
+          components={{
+            Actor: <span className="font-bold capitalize" />,
+          }}
+          values={{ actor: user ? getFullName(user) : 'Workflow' }}
+        />
+      </span>
+      <EventTime time={event.createdAt} />
+    </div>
+  );
 };
