f8c7e7deb92d170d4b6cb84de1b6a143cfe3ba82
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-05-12 15:22:23 +0200

    fix(case-manager): provide key to subcomponents to force state reset

--- packages/app-builder/src/components/CaseManager/PivotsPanel/PivotsPanel.tsx
+++ packages/app-builder/src/components/CaseManager/PivotsPanel/PivotsPanel.tsx
@@ -48,7 +48,6 @@ export function PivotsPanel(props: PivotsPanelProps) {
         <div className="w-[519px] p-8 pt-0">
           <PivotsPanelContent
             currentUser={props.currentUser}
-            key={props.case.id}
             case={props.case}
             pivotObjects={props.pivotObjects}
             dataModel={props.dataModel}
