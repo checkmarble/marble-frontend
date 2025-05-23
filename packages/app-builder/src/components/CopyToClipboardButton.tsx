900d75a512352d7677d1689570bd8ac05a02eb11
Kevin Abatan <abatan.k@gmail.com>
2025-05-23 13:31:16 +0200

    feat: add decision ID to the decision panel

--- packages/app-builder/src/components/CopyToClipboardButton.tsx
+++ packages/app-builder/src/components/CopyToClipboardButton.tsx
@@ -1,12 +1,17 @@
 import { useGetCopyToClipboard } from '@app-builder/utils/use-get-copy-to-clipboard';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { type ComponentPropsWithoutRef, forwardRef } from 'react';
+import { cn } from 'ui-design-system';
 import { Icon } from 'ui-icons';
 
 const variances = cva(
-  'border-grey-90 text-grey-00 hover:bg-grey-95 active:bg-grey-90 flex min-h-8 w-fit shrink-0 cursor-pointer select-none items-center gap-3 break-all rounded border px-2 font-normal transition-colors',
+  'border-grey-90 text-grey-00 hover:bg-grey-95 active:bg-grey-90 flex w-fit shrink-0 cursor-pointer select-none items-center break-all rounded border font-normal transition-colors',
   {
     variants: {
+      size: {
+        sm: 'p-0.5 gap-2',
+        lg: 'min-h-8 gap-3 px-2',
+      },
       dimmed: {
         true: 'text-grey-50',
         false: null,
@@ -14,6 +19,7 @@ const variances = cva(
     },
     defaultVariants: {
       dimmed: false,
+      size: 'lg',
     },
   },
 );
@@ -24,17 +30,23 @@ export type CopyToClipboardButtonProps = ComponentPropsWithoutRef<'button'> &
   };
 
 export const CopyToClipboardButton = forwardRef<HTMLButtonElement, CopyToClipboardButtonProps>(
-  function CopyToClipboardButton({ children, className, toCopy, dimmed, ...props }, ref) {
+  function CopyToClipboardButton({ children, className, toCopy, dimmed, size, ...props }, ref) {
     const getCopyToClipboardProps = useGetCopyToClipboard();
     return (
       <button
         ref={ref}
-        className={variances({ dimmed, className })}
+        className={variances({ dimmed, className, size })}
         {...getCopyToClipboardProps(toCopy)}
         {...props}
       >
         {children}
-        <Icon icon="copy" className="size-4 shrink-0 text-current" />
+        <Icon
+          icon="copy"
+          className={cn('shrink-0 text-current', {
+            'size-4': size === undefined || size === 'lg',
+            'size-3': size === 'sm',
+          })}
+        />
       </button>
     );
   },
