23b1020fcc4979b035c53ecf1c1b611daefc67d5
Kevin Abatan <abatan.k@gmail.com>
2025-05-28 13:37:43 +0200

    feat: adding tags and filtering to list of dataset

---
+++ packages/app-builder/src/components/Sanctions/DatasetTagSelect.tsx
@@ -0,0 +1,80 @@
+import { toggle } from 'radash';
+import { type ComponentProps, type Dispatch, type SetStateAction, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, Checkbox, MenuCommand } from 'ui-design-system';
+import { Icon } from 'ui-icons';
+
+import { DatasetTag } from './DatasetTag';
+
+const Badge = ({ children }: ComponentProps<'span'>) => (
+  <span className="bg-purple-65 text-grey-100 text-2xs rounded px-1 py-0.5">{children}</span>
+);
+
+type DatasetTagSelectProps = {
+  tags: string[];
+  selectedTags: string[];
+  setSelectedTags: Dispatch<SetStateAction<string[]>>;
+};
+
+export const DatasetTagSelect = ({
+  tags,
+  selectedTags,
+  setSelectedTags,
+}: DatasetTagSelectProps) => {
+  const { t } = useTranslation();
+
+  const filteredTags = useMemo(() => tags.filter((t) => t !== ''), [tags]);
+
+  return (
+    <MenuCommand.Menu>
+      <MenuCommand.Trigger>
+        <Button variant="secondary" size="xs">
+          <Icon icon="add-circle" className="size-3.5" />
+          <span className="text-xs">Type</span>
+          {selectedTags.length > 0 ? <div className="bg-grey-80 mx-1 h-3 w-px" /> : null}
+          {selectedTags.length >= 4 ? (
+            <Badge>
+              {t('cases:case_detail.history.nb_selected', { count: selectedTags.length })}
+            </Badge>
+          ) : (
+            selectedTags.map((tag) => <DatasetTag size="sm" key={tag} tag={tag} />)
+          )}
+        </Button>
+      </MenuCommand.Trigger>
+      <MenuCommand.Content className="mt-2 max-h-[400px] max-w-[210px]" align="end">
+        <MenuCommand.Combobox className="m-1 mb-0 h-8 p-0" iconClasses="size-4" />
+        <MenuCommand.List className="p-1">
+          {filteredTags.map((tag) => (
+            <MenuCommand.Item
+              onSelect={() => setSelectedTags((prev) => toggle(prev, tag))}
+              className="flex min-h-0 cursor-pointer items-center justify-start p-1.5"
+              key={tag}
+              value={tag}
+            >
+              <Checkbox size="small" checked={selectedTags.includes(tag)} />
+              <DatasetTag tag={tag} />
+            </MenuCommand.Item>
+          ))}
+          <div className="bg-grey-100 sticky bottom-0 flex w-full gap-2">
+            <Button
+              variant="secondary"
+              size="small"
+              className="basis-full"
+              onClick={() => setSelectedTags([])}
+            >
+              <Icon icon="filters-off" className="size-4" />
+            </Button>
+            <Button
+              variant="secondary"
+              size="small"
+              className="basis-full"
+              onClick={() => setSelectedTags(filteredTags)}
+            >
+              <Icon icon="checked" className="size-3.5" />
+            </Button>
+          </div>
+        </MenuCommand.List>
+      </MenuCommand.Content>
+    </MenuCommand.Menu>
+  );
+};
