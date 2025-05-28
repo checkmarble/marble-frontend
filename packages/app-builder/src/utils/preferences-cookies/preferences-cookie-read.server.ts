b945e3bfbd25556cd495c42d80fb533b236a4990
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-05-28 11:27:49 +0200

    feat: catch cookie parsing errors in Sentry

--- packages/app-builder/src/utils/preferences-cookies/preferences-cookie-read.server.ts
+++ packages/app-builder/src/utils/preferences-cookies/preferences-cookie-read.server.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/remix';
+
 import { COOKIE_NAME, type PreferencesCookie, PreferencesCookieSchema } from './config';
 
 type PreferencesCookieKey = keyof PreferencesCookie;
@@ -10,16 +12,17 @@ export function getPreferencesCookie<K extends PreferencesCookieKey>(
     .get('Cookie')
     ?.split('; ')
     .map((cookie) => cookie.split('='))
-    .map(([key, value]) => [key, decodeURIComponent(value ?? '').replace(/"/g, '')])
+    .map(([key, value]) => [key, decodeURIComponent(value ?? '')])
     .filter(([key]) => key === COOKIE_NAME)[0]?.[1];
 
   if (!rawValue) return undefined;
 
-  let parsedObj: Record<string, unknown> = {};
+  let parsedObj: Record<string, K>;
   try {
     parsedObj = JSON.parse(rawValue);
-  } catch {
-    parsedObj = { [name]: rawValue };
+  } catch (error) {
+    Sentry.captureException(error);
+    parsedObj = { [name]: rawValue as K };
   }
 
   try {
