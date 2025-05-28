7e8ed6cde21fd34938cfdb083bb34b5353f087a0
Kevin Abatan <abatan.k@gmail.com>
2025-05-28 13:37:43 +0200

    feat: adding tags and filtering to list of dataset

--- packages/app-builder/src/models/sanction-check-dataset.ts
+++ packages/app-builder/src/models/sanction-check-dataset.ts
@@ -1,32 +1,4 @@
-import { type OpenSanctionsCatalogDto, type OpenSanctionsDatasetFreshnessDto } from 'marble-api';
-
-export type OpenSanctionsCatalogDataset = {
-  name: string;
-  title: string;
-};
-
-export type OpenSanctionsCatalogSection = {
-  name: string;
-  title: string;
-  datasets: OpenSanctionsCatalogDataset[];
-};
-
-export type OpenSanctionsCatalog = {
-  sections: OpenSanctionsCatalogSection[];
-};
-
-export function adaptOpenSanctionsCatalog(catalog: OpenSanctionsCatalogDto): OpenSanctionsCatalog {
-  return {
-    sections: catalog.sections.map((s) => ({
-      name: s.name,
-      title: s.title,
-      datasets: s.datasets.map((d) => ({
-        name: d.name,
-        title: d.title,
-      })),
-    })),
-  };
-}
+import { type OpenSanctionsDatasetFreshnessDto } from 'marble-api';
 
 export type OpenSanctionsUpstreamDatasetFreshness = {
   version: string;
