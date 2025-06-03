5419a7158e3d97fe50265df5d65d7dc53fced887
Kevin Abatan <abatan.k@gmail.com>
2025-06-03 16:16:55 +0200

    refactor: add translations

--- packages/app-builder/src/components/Sanctions/DatasetTag.tsx
+++ packages/app-builder/src/components/Sanctions/DatasetTag.tsx
@@ -1,17 +1,28 @@
+import { useTranslation } from 'react-i18next';
+import { match } from 'ts-pattern';
 import { cn } from 'ui-design-system';
 
-export const DatasetTag = ({ tag }: { tag: string }) => (
-  <span
-    className={cn(
-      'bg-grey-95 text-grey-50 text-2xs shrink-0 rounded-full px-2 py-[3px] font-medium',
-      {
-        'bg-orange-95 text-orange-50': tag === 'peps',
-        'bg-blue-96 text-blue-58': tag === 'third-parties',
-        'bg-red-95 text-red-47': tag === 'negative-news',
-        'bg-yellow-90 text-yellow-50': tag === 'adverse-media',
-      },
-    )}
-  >
-    {tag}
-  </span>
-);
+export const DatasetTag = ({ tag }: { tag: string }) => {
+  const { t } = useTranslation(['scenarios']);
+
+  return (
+    <span
+      className={cn(
+        'bg-grey-95 text-grey-50 text-2xs shrink-0 rounded-full px-2 py-[3px] font-medium',
+        {
+          'bg-orange-95 text-orange-50': tag === 'peps',
+          'bg-blue-96 text-blue-58': tag === 'third-parties',
+          'bg-red-95 text-red-47': tag === 'negative-news',
+          'bg-yellow-90 text-yellow-50': tag === 'adverse-media',
+        },
+      )}
+    >
+      {match(tag)
+        .with('peps', () => t(`scenarios:sanction.lists.peps`))
+        .with('third-parties', () => t(`scenarios:sanction.lists.third_parties`))
+        .with('adverse-media', () => t(`scenarios:sanction.lists.adverse_media`))
+        .with('negative-news', () => t(`scenarios:sanction.lists.negative_news`))
+        .otherwise(() => t(`scenarios:sanction.lists.other`))}
+    </span>
+  );
+};
