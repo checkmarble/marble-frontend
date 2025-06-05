1444fa7f59d2783193ed263d5ab1fc07d428c957
Kevin Abatan <abatan.k@gmail.com>
2025-06-05 17:02:39 +0200

    fix: untagged list was labeled other

--- packages/app-builder/src/components/Sanctions/DatasetTag.tsx
+++ packages/app-builder/src/components/Sanctions/DatasetTag.tsx
@@ -7,22 +7,22 @@ export const DatasetTag = ({ tag }: { tag: string }) => {
 
   return (
     <span
-      className={cn(
-        'bg-grey-95 text-grey-50 text-2xs shrink-0 rounded-full px-2 py-[3px] font-medium',
-        {
-          'bg-orange-95 text-orange-50': tag === 'peps',
-          'bg-blue-96 text-blue-58': tag === 'third-parties',
-          'bg-red-95 text-red-47': tag === 'negative-news',
-          'bg-yellow-90 text-yellow-50': tag === 'adverse-media',
-        },
-      )}
+      className={cn('text-2xs shrink-0 rounded-full px-2 py-[3px] font-medium', {
+        'bg-orange-95 text-orange-50': tag === 'peps',
+        'bg-blue-96 text-blue-58': tag === 'third-parties',
+        'bg-red-95 text-red-47': tag === 'negative-news',
+        'bg-yellow-90 text-yellow-50': tag === 'adverse-media',
+        'bg-grey-95 text-grey-50': tag === 'sanctions',
+      })}
     >
       {match(tag)
         .with('peps', () => t(`scenarios:sanction.lists.peps`))
         .with('third-parties', () => t(`scenarios:sanction.lists.third_parties`))
+        .with('sanctions', () => t(`scenarios:sanction.lists.sanctions`))
         .with('adverse-media', () => t(`scenarios:sanction.lists.adverse_media`))
         .with('negative-news', () => t(`scenarios:sanction.lists.negative_news`))
-        .otherwise(() => t(`scenarios:sanction.lists.other`))}
+        .with('other', () => t(`scenarios:sanction.lists.other`))
+        .otherwise(() => '')}
     </span>
   );
 };
