6c3567ceb17eabb4a47967ae1e7b61aa25a7c8e5
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-06-20 15:50:29 +0200

    fix(lint): ignore lint/suspicious/noShadowRestrictedNames

--- packages/app-builder/src/models/cases.ts
+++ packages/app-builder/src/models/cases.ts
@@ -11,6 +11,7 @@ import {
   type CaseTagDto,
   type CommentEntityAnnotationDto,
   type CreateCaseBodyDto,
+  // biome-ignore lint/suspicious/noShadowRestrictedNames: <TBD>
   type Error,
   type FileEntityAnnotationDto,
   type Outcome,
