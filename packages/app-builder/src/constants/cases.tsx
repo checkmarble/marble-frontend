1cefb809a833efc91831cd2f7d1e1c163be48599
Pascal Delange <pascal@checkmarble.com>
2025-06-30 13:50:05 +0200

    revamp fr trads for case manager envent categories

--- packages/app-builder/src/constants/cases.tsx
+++ packages/app-builder/src/constants/cases.tsx
@@ -1,17 +1,16 @@
 import { CaseEventType } from '@app-builder/models/cases';
 
 export const CASE_EVENT_CATEGORIES = [
-  'case_lifecycle',
-  'case_assignation',
-  'case_result',
-  'sar_related',
   'case_review_action',
+  'case_assignation',
+  'sar_related',
+  'others',
 ] as const;
 
 export type CaseEventCategory = (typeof CASE_EVENT_CATEGORIES)[number];
 
 export const CASE_EVENT_CATEGORY_TO_EVENTS_MAPPING = {
-  case_lifecycle: [
+  others: [
     'case_created',
     'decision_reviewed',
     'name_updated',
@@ -19,9 +18,10 @@ export const CASE_EVENT_CATEGORY_TO_EVENTS_MAPPING = {
     'status_updated',
     'case_snoozed',
     'case_unsnoozed',
+    'rule_snooze_created',
+    'outcome_updated',
   ],
   case_assignation: ['inbox_changed', 'case_assigned'],
-  case_result: ['rule_snooze_created', 'outcome_updated'],
   sar_related: ['sar_created', 'sar_deleted', 'sar_status_changed', 'sar_file_uploaded'],
   case_review_action: ['comment_added', 'file_added', 'decision_reviewed', 'entity_annotated'],
 } as const satisfies Record<CaseEventCategory, CaseEventType[]>;
