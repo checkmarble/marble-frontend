eebadc70eb2851b63a684641e714c7b3f2e7d569
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-06-30 14:59:58 +0200

    feat: make family details collapsible

--- packages/app-builder/src/components/Sanctions/MatchCard/FamilyDetail.tsx
+++ packages/app-builder/src/components/Sanctions/MatchCard/FamilyDetail.tsx
@@ -2,6 +2,7 @@ import { FamilyPersonEntity } from '@app-builder/models/sanction-check';
 import { formatDateTimeWithoutPresets, useFormatLanguage } from '@app-builder/utils/format';
 import { useTranslation } from 'react-i18next';
 import * as R from 'remeda';
+import { Collapsible } from 'ui-design-system';
 import { TopicTag } from '../TopicTag';
 
 export const FamilyDetail = ({ familyMembers }: { familyMembers: FamilyPersonEntity[] }) => {
@@ -10,72 +11,76 @@ export const FamilyDetail = ({ familyMembers }: { familyMembers: FamilyPersonEnt
   const { t } = useTranslation(['sanctions']);
 
   return (
-    <div className="grid grid-cols-[168px,_1fr] gap-2">
-      {familyMembers.map((member, memberIndex) => {
-        return member.properties.relative?.map(({ id, properties }, idx) => {
-          if (!properties.name?.[0]) return null;
-          const rel =
-            member.properties.relationship
-              ?.map((relation) =>
-                t(`sanctions:relation.${R.toCamelCase(relation)}.label`, {
-                  defaultValue: relation,
-                }),
-              )
-              .join(' · ') ?? t('sanctions:match.family.unknown_relationship');
+    <div className="grid grid-cols-[168px,_1fr] gap-y-2">
+      <div className="font-bold">{t('sanctions:match.family-members.title')}</div>
+      <Collapsible.Container defaultOpen={familyMembers.length <= 3}>
+        <Collapsible.Title>
+          {t('sanctions:match.family-member.count', { count: familyMembers.length })}
+        </Collapsible.Title>
+        <Collapsible.Content>
+          <div className="flex flex-col gap-2">
+            {familyMembers.map((member, memberIndex) => {
+              return member.properties.relative?.map(({ id, properties }, idx) => {
+                if (!properties.name?.[0]) return null;
+                const rel =
+                  member.properties.relationship
+                    ?.map((relation) =>
+                      t(`sanctions:relation.${R.toCamelCase(relation)}.label`, {
+                        defaultValue: relation,
+                      }),
+                    )
+                    .join(' · ') ?? t('sanctions:match.family.unknown_relationship');
 
-          const isFirstElement = memberIndex === 0 && idx === 0;
-
-          return (
-            <div key={`person-${id}-${idx}`} className="contents">
-              <div className="font-semibold">
-                {isFirstElement && (
-                  <div className="font-bold mb-2">{t('sanctions:match.family-members.title')}</div>
-                )}
-              </div>
-              <div className="flex flex-row items-start  gap-2 rounded p-2 bg-grey-100">
-                <div className="flex flex-col  gap-2">
-                  <div className="col-span-full flex w-full flex-wrap gap-1">
-                    <span>
-                      {properties.firstName?.slice(0, 3).join(' ')} {properties['secondName']?.[0]}
-                    </span>
-                    <span className="font-semibold">
-                      {properties.lastName?.slice(0, 3).join(' ') ?? 'unknown'}
-                    </span>
+                return (
+                  <div key={`person-${id}-${idx}`} className="contents">
+                    <div className="flex flex-row items-start  gap-2 rounded p-2 bg-grey-100">
+                      <div className="flex flex-col  gap-2">
+                        <div className="col-span-full flex w-full flex-wrap gap-1">
+                          <span>
+                            {properties.firstName?.slice(0, 3).join(' ')}{' '}
+                            {properties['secondName']?.[0]}
+                          </span>
+                          <span className="font-semibold">
+                            {properties.lastName?.slice(0, 3).join(' ') ?? 'unknown'}
+                          </span>
+                        </div>
+                        <div className="text-sm text-grey-70 font-medium">
+                          {rel}
+                          {member.properties.startDate?.[0] && (
+                            <span>
+                              {' '}
+                              (
+                              {formatDateTimeWithoutPresets(member.properties.startDate[0], {
+                                language,
+                                dateStyle: 'medium',
+                              })}
+                              {member.properties.endDate?.[0] && (
+                                <>
+                                  {' - '}
+                                  {formatDateTimeWithoutPresets(member.properties.endDate[0], {
+                                    language,
+                                    dateStyle: 'medium',
+                                  })}
+                                </>
+                              )}
+                              )
+                            </span>
+                          )}
+                        </div>
+                        <div className="col-span-full flex w-full flex-wrap gap-1">
+                          {properties['topics']?.map((topic) => (
+                            <TopicTag key={`${id}-${topic}`} topic={topic} />
+                          ))}
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="text-sm text-grey-70 font-medium">
-                    {rel}
-                    {member.properties.startDate?.[0] && (
-                      <span>
-                        {' '}
-                        (
-                        {formatDateTimeWithoutPresets(member.properties.startDate[0], {
-                          language,
-                          dateStyle: 'medium',
-                        })}
-                        {member.properties.endDate?.[0] && (
-                          <>
-                            {' - '}
-                            {formatDateTimeWithoutPresets(member.properties.endDate[0], {
-                              language,
-                              dateStyle: 'medium',
-                            })}
-                          </>
-                        )}
-                        )
-                      </span>
-                    )}
-                  </div>
-                  <div className="col-span-full flex w-full flex-wrap gap-1">
-                    {properties['topics']?.map((topic) => (
-                      <TopicTag key={`${id}-${topic}`} topic={topic} />
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        });
-      })}
+                );
+              });
+            })}
+          </div>
+        </Collapsible.Content>
+      </Collapsible.Container>
     </div>
   );
 };
