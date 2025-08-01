be8cd0ed56f8b603e0b38afc09d9b5dc1bed59ad
Christopher Debove <christopher@checkmarble.com>
2025-08-01 09:20:42 +0200

    fix(config): make it possible for the frontend to configure firebase emulator

--- packages/app-builder/src/utils/environment.ts
+++ packages/app-builder/src/utils/environment.ts
@@ -28,7 +28,7 @@ const PublicEnvVarsSchema = z.object({
 
   METABASE_URL: z.string().optional(),
 
-  // FIREBASE_AUTH_EMULATOR_HOST: z.string().optional(),
+  FIREBASE_AUTH_EMULATOR_HOST: z.string().optional(),
   FIREBASE_API_KEY: z.string().optional(),
   FIREBASE_AUTH_DOMAIN: z.string().optional(),
   FIREBASE_PROJECT_ID: z.string().optional(),
@@ -85,7 +85,7 @@ interface ServerEnvVars {
   SESSION_MAX_AGE?: string;
   MARBLE_API_URL_SERVER: string;
   MARBLE_APP_URL: string;
-  FIREBASE_CONFIG: FirebaseOptions;
+  FIREBASE_CONFIG: FirebaseConfig;
   METABASE_URL?: string;
   SENTRY_DSN?: string;
   SENTRY_ENVIRONMENT?: string;
@@ -145,12 +145,20 @@ export function getClientEnv<K extends keyof ClientEnvVars>(clientEnvVarName: K)
   return clientEnv[clientEnvVarName];
 }
 
-function parseFirebaseConfigFromEnv(): FirebaseOptions {
+export type FirebaseConfig = {
+  emulatorHost?: string;
+  options: FirebaseOptions;
+};
+
+function parseFirebaseConfigFromEnv(): FirebaseConfig {
   const options: FirebaseOptions = {
     apiKey: getEnv('FIREBASE_API_KEY'),
     authDomain: getEnv('FIREBASE_AUTH_DOMAIN'),
     projectId: getEnv('FIREBASE_PROJECT_ID'),
   };
 
-  return options;
+  return {
+    emulatorHost: getEnv('FIREBASE_AUTH_EMULATOR_HOST'),
+    options,
+  };
 }
