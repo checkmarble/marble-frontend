2c842ec5cf240ca7e0d2899d5e30cfe21bce14ee
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-08-22 00:26:49 +0200

    fix: unsnooze case on button click

--- packages/app-builder/src/routes/ressources+/cases+/snooze-case.tsx
+++ packages/app-builder/src/routes/ressources+/cases+/snooze-case.tsx
@@ -15,7 +15,7 @@ import {
   nextMonday,
   startOfHour,
 } from 'date-fns';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { match } from 'ts-pattern';
 import { Button, Calendar, cn, MenuCommand } from 'ui-design-system';
@@ -114,13 +114,7 @@ export function SnoozeCase({
     } as EditSnoozeForm,
   });
 
-  const selectedDate = useStore(form.store, (state) => state.values.snoozeUntil);
-
-  const isCustomDate = useMemo(() => {
-    if (!selectedDate) return false;
-    const availableDates = getDurations().map(({ date }) => date);
-    return !availableDates.some((date) => isSameDay(date, new Date(selectedDate)));
-  }, [selectedDate]);
+  useStore(form.store, (state) => state.values.snoozeUntil);
 
   return (
     <form.Field
@@ -130,68 +124,55 @@ export function SnoozeCase({
         onChange: editSnoozeSchema.shape.snoozeUntil,
       }}
     >
-      {(field) => (
-        <MenuCommand.Menu open={isOpen} onOpenChange={setIsOpen}>
-          <MenuCommand.Trigger>
-            <Button
-              variant="secondary"
-              size="medium"
-              className={cn({
-                'bg-purple-96': field.state.value,
-              })}
-            >
-              <Icon
-                icon={field.state.value ? 'snooze-on' : 'snooze'}
-                className="size-5"
-                aria-hidden
-              />
-              {field.state.value ? t('cases:unsnooze_case.title') : t('cases:snooze_case.title')}
-            </Button>
-          </MenuCommand.Trigger>
-          <MenuCommand.Content className="mt-2 min-w-[264px]">
-            <MenuCommand.List>
-              {getDurations().map(({ duration, date }) => (
-                <MenuCommand.Item
-                  onSelect={() => {
-                    field.handleChange(
-                      field.state.value === date.toISOString() ? null : date.toISOString(),
-                    );
-                    form.handleSubmit();
-                  }}
-                  key={duration}
-                >
-                  <span className="text-r inline-flex items-center gap-1">
-                    <span>
-                      {match(duration)
-                        .with('tomorrow', () => t('common:snooze.tomorrow'))
-                        .with('oneWeek', () => t('common:snooze.oneWeek'))
-                        .with('oneMonth', () => t('common:snooze.oneMonth'))
-                        .with('nextMonday', () => t('common:snooze.nextMonday'))
-                        .exhaustive()}
+      {(field) =>
+        field.state.value ? (
+          <Button
+            variant="secondary"
+            size="medium"
+            className={cn({
+              'bg-purple-96': field.state.value,
+            })}
+            onClick={() => {
+              field.handleChange(null);
+              form.handleSubmit();
+            }}
+          >
+            <Icon icon="snooze-on" className="size-5" aria-hidden />
+            {t('cases:unsnooze_case.title')}
+          </Button>
+        ) : (
+          <MenuCommand.Menu open={isOpen} onOpenChange={setIsOpen}>
+            <MenuCommand.Trigger>
+              <Button variant="secondary" size="medium">
+                <Icon icon="snooze" className="size-5" aria-hidden />
+                {t('cases:snooze_case.title')}
+              </Button>
+            </MenuCommand.Trigger>
+            <MenuCommand.Content className="mt-2 min-w-[264px]">
+              <MenuCommand.List>
+                {getDurations().map(({ duration, date }) => (
+                  <MenuCommand.Item
+                    onSelect={() => {
+                      field.handleChange(
+                        field.state.value === date.toISOString() ? null : date.toISOString(),
+                      );
+                      form.handleSubmit();
+                    }}
+                    key={duration}
+                  >
+                    <span className="text-r inline-flex items-center gap-1">
+                      <span>
+                        {match(duration)
+                          .with('tomorrow', () => t('common:snooze.tomorrow'))
+                          .with('oneWeek', () => t('common:snooze.oneWeek'))
+                          .with('oneMonth', () => t('common:snooze.oneMonth'))
+                          .with('nextMonday', () => t('common:snooze.nextMonday'))
+                          .exhaustive()}
+                      </span>
+                      <span className="text-2xs text-grey-50">{formatDate(date)}</span>
                     </span>
-                    <span className="text-2xs text-grey-50">{formatDate(date)}</span>
-                  </span>
-                  {field.state.value && isSameDay(date, new Date(field.state.value)) ? (
-                    <Icon icon="tick" className="text-purple-65 size-6" />
-                  ) : null}
-                </MenuCommand.Item>
-              ))}
-              {field.state.value && isCustomDate ? (
-                <MenuCommand.Item
-                  onSelect={() => {
-                    field.handleChange(null);
-                    form.handleSubmit();
-                  }}
-                >
-                  <span className="text-r inline-flex h-full items-center gap-1">
-                    <span>{t('common:snooze.custom')}</span>
-                    <span className="text-2xs text-grey-50">
-                      {formatDate(new Date(field.state.value))}
-                    </span>
-                  </span>
-                  <Icon icon="tick" className="text-purple-65 size-6" />
-                </MenuCommand.Item>
-              ) : (
+                  </MenuCommand.Item>
+                ))}
                 <MenuCommand.SubMenu
                   arrow={false}
                   hover={false}
@@ -199,15 +180,7 @@ export function SnoozeCase({
                     <>
                       <span className="text-r inline-flex h-full items-center gap-1">
                         <span>{t('common:snooze.custom')}</span>
-                        {field.state.value && isCustomDate ? (
-                          <span className="text-2xs text-grey-50">
-                            {formatDate(new Date(field.state.value))}
-                          </span>
-                        ) : null}
                       </span>
-                      {field.state.value && isCustomDate ? (
-                        <Icon icon="tick" className="text-purple-65 size-6" />
-                      ) : null}
                     </>
                   }
                 >
@@ -229,11 +202,11 @@ export function SnoozeCase({
                     />
                   </MenuCommand.List>
                 </MenuCommand.SubMenu>
-              )}
-            </MenuCommand.List>
-          </MenuCommand.Content>
-        </MenuCommand.Menu>
-      )}
+              </MenuCommand.List>
+            </MenuCommand.Content>
+          </MenuCommand.Menu>
+        )
+      }
     </form.Field>
   );
 }
