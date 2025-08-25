3cf8fdb9b585ab07c2a4cd54801ba2abd1641396
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-08-25 15:57:29 +0200

    feat: remove unsnooze on comment post

--- packages/app-builder/src/routes/ressources+/cases+/add-comment.tsx
+++ packages/app-builder/src/routes/ressources+/cases+/add-comment.tsx
@@ -17,7 +17,6 @@ import {
 } from '@remix-run/node';
 import { redirect, useFetcher } from '@remix-run/react';
 import { useForm } from '@tanstack/react-form';
-import { isBefore, parseISO } from 'date-fns';
 import { decode } from 'decode-formdata';
 import { serialize } from 'object-to-formdata';
 import { toggle, tryit } from 'radash';
@@ -102,16 +101,6 @@ export async function action({ request }: ActionFunctionArgs) {
         }),
       );
     }
-
-    promises.push(
-      cases
-        .getCase({ caseId: data.caseId })
-        .then(({ snoozedUntil }) =>
-          snoozedUntil && isBefore(new Date(), parseISO(snoozedUntil))
-            ? cases.unsnoozeCase({ caseId: data.caseId })
-            : Promise.resolve(),
-        ),
-    );
 
     await Promise.all(promises);
 
