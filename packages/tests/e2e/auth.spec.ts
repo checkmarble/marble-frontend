ddd94114d05100f7ac658e0e3377d32ae9f6e87b
Debove Christopher <chibiblasphem@gmail.com>
2025-08-27 13:31:25 +0200

    ci: add e2e testing with playwright (#954)
    * Add basic Playwright tests.
    
    * Add DB fixtures for data model, and simple E2E test for the AST builder.
    
    * improve playwright tests
    
    ---------
    
    Co-authored-by: Antoine Popineau <antoine.popineau@checkmarble.com>

---
+++ packages/tests/e2e/auth.spec.ts
@@ -0,0 +1,65 @@
+import { expect, test } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+test('Initial login page', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForURL('/sign-in-email?email=');
+
+  await expect(page).toHaveTitle('Marble');
+  await expect(page.getByText('Iterate. Improve. Automate.')).toBeVisible();
+});
+
+test('Invalid username', async ({ page }) => {
+  await page.goto('/sign-in');
+
+  await page
+    .getByRole('textbox')
+    .and(page.locator('[name="credentials.email"]'))
+    .fill('invalid@zorg.com');
+  await page
+    .getByRole('textbox')
+    .and(page.locator('[name="credentials.password"]'))
+    .fill('very-secret');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+  await expect(page.getByText('Invalid login credentials.')).toBeVisible();
+});
+
+test('Invalid password', async ({ page }) => {
+  await page.goto('/sign-in');
+
+  await page
+    .getByRole('textbox')
+    .and(page.locator('[name="credentials.email"]'))
+    .fill('jbe@zorg.com');
+  await page
+    .getByRole('textbox')
+    .and(page.locator('[name="credentials.password"]'))
+    .fill('invalid');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+  await expect(page.getByText('Invalid login credentials.')).toBeVisible();
+});
+
+const authState = path.join(path.dirname(fileURLToPath(import.meta.url)), '../auth.json');
+
+test('Authentication', async ({ page }) => {
+  await page.goto('/sign-in');
+
+  await page
+    .getByRole('textbox')
+    .and(page.locator('[name="credentials.email"]'))
+    .fill('jbe@zorg.com');
+  await page
+    .getByRole('textbox')
+    .and(page.locator('[name="credentials.password"]'))
+    .fill('very-secret');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+  await page.waitForURL('/scenarios');
+
+  await expect(page.locator('nav').getByRole('link', { name: 'Scenarios ' })).toBeVisible();
+
+  await page.context().storageState({ path: authState, indexedDB: true });
+});
---
+++ packages/tests/e2e/setup.ts
@@ -0,0 +1,83 @@
+import { GenericContainer, Network, Wait } from 'testcontainers';
+
+import { setupFixtures } from './fixtures';
+
+async function globalSetup() {
+  if (process.env['PW_SETUP_DONE']) return;
+
+  const net = await new Network().start();
+
+  const db = await new GenericContainer('postgres:15')
+    .withNetwork(net)
+    .withNetworkAliases('db')
+    .withEnvironment({
+      POSTGRES_PASSWORD: 'marble',
+      POSTGRES_DB: 'marble',
+    })
+    .withExposedPorts(5432)
+    .withWaitStrategy(Wait.forListeningPorts())
+    .withDefaultLogDriver()
+    .start();
+
+  const dsn = 'postgres://postgres:marble@db/marble?sslmode=disable';
+  const externalDsn = `postgres://postgres:marble@127.0.0.1:${db.getFirstMappedPort()}/marble?sslmode=disable`;
+
+  const firebase = await new GenericContainer(
+    'europe-west1-docker.pkg.dev/marble-infra/marble/firebase-emulator:latest',
+  )
+    .withNetwork(net)
+    .withNetworkAliases('firebase')
+    .withExposedPorts(9099)
+    .withWaitStrategy(Wait.forListeningPorts())
+    .withDefaultLogDriver()
+    .start();
+
+  await new GenericContainer('europe-west1-docker.pkg.dev/marble-infra/marble/marble-backend')
+    .withPlatform('linux/x86_64')
+    .withNetwork(net)
+    .withEnvironment({
+      PG_CONNECTION_STRING: dsn,
+    })
+    .withCommand(['-migrations'])
+    .withWaitStrategy(Wait.forLogMessage('successfully migrated'))
+    .withDefaultLogDriver()
+    .start();
+
+  const api = await new GenericContainer(
+    'europe-west1-docker.pkg.dev/marble-infra/marble/marble-backend',
+  )
+    .withPlatform('linux/x86_64')
+    .withNetwork(net)
+    .withNetworkAliases('api')
+    .withEnvironment({
+      PG_CONNECTION_STRING: dsn,
+      MARBLE_APP_URL: 'http://localhost:3000',
+      FIREBASE_AUTH_EMULATOR_HOST: `firebase:9099`,
+      FIREBASE_API_KEY: 'dummy',
+      GOOGLE_CLOUD_PROJECT: 'test-project',
+      CREATE_GLOBAL_ADMIN_EMAIL: 'admin@checkmarble.com',
+      CREATE_ORG_NAME: 'Zorg',
+      CREATE_ORG_ADMIN_EMAIL: 'jbe@zorg.com',
+    })
+    .withExposedPorts(8080)
+    .withCommand(['-server'])
+    .withWaitStrategy(Wait.forHttp('/liveness', 8080, { abortOnContainerExit: true }))
+    .withDefaultLogDriver()
+    .start();
+
+  process.env['API_PORT'] = api.getFirstMappedPort().toString();
+  process.env['FIREBASE_PORT'] = firebase.getFirstMappedPort().toString();
+
+  await setupFixtures(externalDsn, process.env['API_PORT']);
+
+  process.on('exit', async () => {
+    await api.stop({ timeout: 1 });
+    await firebase.stop({ timeout: 1 });
+    await db.stop({ timeout: 1 });
+    await net.stop();
+  });
+
+  process.env['PW_SETUP_DONE'] = 'true';
+}
+
+export default globalSetup;
