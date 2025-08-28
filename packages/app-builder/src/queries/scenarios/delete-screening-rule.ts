3f16c0b63bfe76dc30ea062779e64a01a9bd5684
Christopher Debove <christopher@checkmarble.com>
2025-08-28 16:37:50 +0200

    refactor(resources/scenarios): migrate scenario/iterations/sanction fetchers

---
+++ packages/app-builder/src/queries/scenarios/delete-screening-rule.ts
@@ -0,0 +1,33 @@
+import { useAgnosticNavigation } from '@app-builder/contexts/AgnosticNavigationContext';
+import { getRoute } from '@app-builder/utils/routes';
+import { fromUUIDtoSUUID } from '@app-builder/utils/short-uuid';
+import { useMutation } from '@tanstack/react-query';
+
+const endpoint = (scenarioId: string, iterationId: string, screeningId: string) =>
+  getRoute('/ressources/scenarios/:scenarioId/:iterationId/sanctions/:sanctionId/delete', {
+    scenarioId: fromUUIDtoSUUID(scenarioId),
+    iterationId: fromUUIDtoSUUID(iterationId),
+    sanctionId: fromUUIDtoSUUID(screeningId),
+  });
+
+export const useDeleteScreeningRuleMutation = (scenarioId: string, iterationId: string) => {
+  const navigate = useAgnosticNavigation();
+
+  return useMutation({
+    mutationKey: ['scenarios', 'iteration', 'delete-screening-rule', scenarioId, iterationId],
+    mutationFn: async (screeningId: string) => {
+      const response = await fetch(endpoint(scenarioId, iterationId, screeningId), {
+        method: 'POST',
+      });
+
+      const result = await response.json();
+
+      if (result.redirectTo) {
+        navigate(result.redirectTo);
+        return;
+      }
+
+      return result;
+    },
+  });
+};
