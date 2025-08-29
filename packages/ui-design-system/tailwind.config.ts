f6c884d517b6546bd2d5cc55657340a3d2beb9a7
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-08-29 18:55:39 +0200

    fix(ui-design-system): cast sharedTailwindConfig to Config type in tailwind.config.ts

--- packages/ui-design-system/tailwind.config.ts
+++ packages/ui-design-system/tailwind.config.ts
@@ -3,6 +3,6 @@ import type { Config } from 'tailwindcss';
 import sharedTailwindConfig from '../tailwind-preset/src/tailwind.config';
 
 export default {
-  presets: [sharedTailwindConfig],
+  presets: [sharedTailwindConfig as unknown as Config],
   content: ['./src/**/*.{ts,tsx,html}'],
 } satisfies Config;
