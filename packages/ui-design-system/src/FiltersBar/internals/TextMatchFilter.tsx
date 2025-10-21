33297fdfc65e9a68e9c2f0381da5ee06b8d3eb73
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-10-21 07:59:46 +0200

    fix: edit modal

--- packages/ui-design-system/src/FiltersBar/internals/TextMatchFilter.tsx
+++ packages/ui-design-system/src/FiltersBar/internals/TextMatchFilter.tsx
@@ -6,7 +6,7 @@ import { FilterItem, FilterPopover } from './FilterPopover';
 import { useFiltersBarContext } from './FiltersBarContext';
 
 export function TextMatchFilter({ filter }: { filter: TextFilter }) {
-  const [isOpen, setOpen] = useState(true);
+  const [isOpen, setOpen] = useState(false);
   const toTextArray = (selected: unknown): string[] => {
     const arr = selected as any[];
     if (!Array.isArray(arr)) return [];
@@ -21,64 +21,67 @@ export function TextMatchFilter({ filter }: { filter: TextFilter }) {
   useEffect(() => {
     if (isOpen) setLocalText(toTextArray(filter.selectedValue));
   }, [isOpen, filter.selectedValue]);
-  if (filter.removable) {
-    return (
-      <FilterPopover.Root open={isOpen} onOpenChange={setOpen}>
-        <FilterItem.Root>
-          <FilterItem.Trigger id={filter.name}>
-            <span className="font-semibold">{filter.name}</span>
+  // if (filter.removable) {
+  return (
+    <FilterPopover.Root open={isOpen} onOpenChange={setOpen}>
+      <FilterItem.Root className={filter.selectedValue ? 'bg-purple-98' : 'bg-grey-98'}>
+        <FilterItem.Trigger
+          id={filter.name}
+          className={filter.selectedValue ? 'text-purple-65' : 'text-grey-90'}
+        >
+          <span className="font-semibold">{filter.name}</span>
+          {filter.selectedValue ? (
             <span className="font-medium">in {localText.join(',')}</span>
-          </FilterItem.Trigger>
-          {filter.removable ? (
-            <FilterItem.Clear
+          ) : null}
+        </FilterItem.Trigger>
+        {filter.selectedValue ? (
+          <FilterItem.Clear
+            onClick={() => {
+              emitRemove(filter.name);
+              setOpen(false);
+            }}
+          />
+        ) : null}
+      </FilterItem.Root>
+      <FilterPopover.Content>
+        <div className="p-4 flex flex-col gap-2 w-80">
+          <Input
+            placeholder={filter.placeholder}
+            value={localText.join(',')}
+            onChange={(e) => setLocalText(e.currentTarget.value.split(','))}
+          />
+          <div className="flex justify-end">
+            <button
+              className={cn('text-s bg-purple-65 text-white rounded-sm px-3 py-1.5 outline-hidden')}
               onClick={() => {
-                emitRemove(filter.name);
+                const value = localText.map((v) => v.trim()).filter((v) => v.length > 0);
+                if (value.length === 0) {
+                  emitRemove(filter.name);
+                  setOpen(false);
+                  return;
+                }
+                emitSet(filter.name, [
+                  {
+                    operator: filter.operator,
+                    value: value.map((v) => v.trim()),
+                  },
+                ]);
+                // const committed = localText
+                //   .map((v) => v.trim())
+                //   .filter((v) => v.length > 0)
+                //   .map((v) => ({ operator: 'in' as const, value: v }));
+                // const payload = committed.length ? committed : null;
+                // emitSet(filter.name, payload);
                 setOpen(false);
               }}
-            />
-          ) : null}
-        </FilterItem.Root>
-        <FilterPopover.Content>
-          <div className="p-4 flex flex-col gap-2 w-80">
-            <Input
-              placeholder={filter.placeholder}
-              value={localText.join(',')}
-              onChange={(e) => setLocalText(e.currentTarget.value.split(','))}
-            />
-            <div className="flex justify-end">
-              <button
-                className={cn(
-                  'text-s bg-purple-65 text-white rounded-sm px-3 py-1.5 outline-hidden',
-                )}
-                onClick={() => {
-                  const value = localText.map((v) => v.trim()).filter((v) => v.length > 0);
-                  if (value.length === 0) {
-                    emitRemove(filter.name);
-                    setOpen(false);
-                    return;
-                  }
-                  emitSet(filter.name, [
-                    {
-                      operator: filter.operator,
-                      value: value.map((v) => v.trim()),
-                    },
-                  ]);
-                  // const committed = localText
-                  //   .map((v) => v.trim())
-                  //   .filter((v) => v.length > 0)
-                  //   .map((v) => ({ operator: 'in' as const, value: v }));
-                  // const payload = committed.length ? committed : null;
-                  // emitSet(filter.name, payload);
-                  setOpen(false);
-                }}
-              >
-                Done
-              </button>
-            </div>
+            >
+              Done
+            </button>
           </div>
-        </FilterPopover.Content>
-      </FilterPopover.Root>
-    );
-  }
-  return <Input placeholder={filter.placeholder} />;
+        </div>
+      </FilterPopover.Content>
+    </FilterPopover.Root>
+  );
+  // }
+  // return <Input placeholder={filter.placeholder} />;
 }
