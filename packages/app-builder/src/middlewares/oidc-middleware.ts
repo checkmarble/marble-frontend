a7547a38d5092473cc8e5b2534939260fbccecaa
Christopher Debove <christopher@checkmarble.com>
2025-10-27 09:47:43 +0100

    fix: redirect oidc routes when firebase provider

--- packages/app-builder/src/middlewares/oidc-middleware.ts
+++ packages/app-builder/src/middlewares/oidc-middleware.ts
@@ -1,9 +1,15 @@
 import { createMiddlewareWithGlobalContext } from '@app-builder/core/requests';
 import { Tokens } from '@app-builder/routes/oidc+/auth';
+import { getRoute } from '@app-builder/utils/routes';
+import { redirect } from '@remix-run/server-runtime';
 
 export const oidcMiddleware = createMiddlewareWithGlobalContext(
   [],
   async function oidcMiddleware({ request, context }, next) {
+    if (context.appConfig.auth.provider !== 'oidc') {
+      throw redirect(getRoute('/sign-in'));
+    }
+
     const oidc = await context.services.authService.makeOidcService(context.appConfig);
     let tokens: Tokens | null = null;
     let error: unknown | null = null;
