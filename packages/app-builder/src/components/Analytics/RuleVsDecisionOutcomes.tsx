1a06255df736f59473e0a31663c47684657cdb98
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-11-06 11:41:16 +0100

    style(analytics): enhance layout of RuleVsDecisionOutcomes (#1197)

--- packages/app-builder/src/components/Analytics/RuleVsDecisionOutcomes.tsx
+++ packages/app-builder/src/components/Analytics/RuleVsDecisionOutcomes.tsx
@@ -97,7 +97,7 @@ export function RuleVsDecisionOutcomes({
       </div>
 
       <div
-        className="bg-white border border-grey-90 rounded-lg p-v2-md shadow-sm mt-v2-sm flex flex-col"
+        className="bg-white border border-grey-90 rounded-lg p-v2-md shadow-sm mt-v2-sm flex flex-col min-h-52"
         style={{ height: data?.length ? data.length * 42 + 120 : '48px' }}
       >
         {isLoading ? (
@@ -172,7 +172,7 @@ export function RuleVsDecisionOutcomes({
             </div>
           </div>
         ) : (
-          <div className="flex items-center justify-center h-full min-h-40">
+          <div className="flex items-center justify-center h-full">
             <span className="text-v2-md text-grey-80">{t('analytics:no_data')}</span>
           </div>
         )}
