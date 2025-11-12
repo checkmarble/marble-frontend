f1c6728498a3ab0f7f6d715546a252cd609aa640
Nicolas Chaintron <nicolas.chaintron@checkmarble.com>
2025-11-12 11:28:18 +0100

    feat(analytics): add DecisionsScoreDistribution

--- packages/app-builder/src/queries/analytics/get-screening-hits-table.ts
+++
@@ -1,4 +0,0 @@
-import { ScreeningHitTableResponse } from '@app-builder/models/analytics';
-import { createAnalyticsQuery } from './factory/create-analytics-query';
-
-export const useGetScreeningHitsTable = createAnalyticsQuery<ScreeningHitTableResponse[]>('screening-hits-table');
