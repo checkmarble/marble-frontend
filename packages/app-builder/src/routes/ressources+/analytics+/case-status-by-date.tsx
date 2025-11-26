998c3b018856efad34a5b09649f1df0d1961e6dc
Christopher Debove <christopher@checkmarble.com>
2025-11-26 17:26:16 +0100

    feat: add error management and graph gridline style

--- packages/app-builder/src/routes/ressources+/analytics+/case-status-by-date.tsx
+++ packages/app-builder/src/routes/ressources+/analytics+/case-status-by-date.tsx
@@ -5,16 +5,7 @@ import { handleRedirectMiddleware } from '@app-builder/middlewares/handle-redire
 export const loader = createServerFn(
   [handleRedirectMiddleware, authMiddleware],
   async function createAnalyticsCaseStatusByDateAction({ context }) {
-    // const data = await context.authInfo.analytics.getCaseStatusByDate();
-    const pow = Math.floor(Math.random() * (6 - 1) + 1);
-    const multiplier = Math.pow(10, pow);
-    const data = Array.from({ length: 10 }, (_, i) => ({
-      date: new Date(new Date().setDate(new Date().getDate() - i)).toISOString().split('T')[0],
-      pending: Math.floor(Math.random() * multiplier),
-      investigating: Math.floor(Math.random() * multiplier),
-      closed: Math.floor(Math.random() * multiplier),
-      snoozed: Math.floor(Math.random() * multiplier),
-    }));
+    const data = await context.authInfo.analytics.getCaseStatusByDate();
     return { casesStatusByDate: data };
   },
 );
--- packages/app-builder/src/routes/ressources+/analytics+/case-status-by-inbox.tsx
+++ packages/app-builder/src/routes/ressources+/analytics+/case-status-by-inbox.tsx
@@ -5,17 +5,7 @@ import { handleRedirectMiddleware } from '@app-builder/middlewares/handle-redire
 export const loader = createServerFn(
   [handleRedirectMiddleware, authMiddleware],
   async function createAnalyticsCaseStatusByDateAction({ context }) {
-    // const data = await context.authInfo.analytics.getCaseStatusByInbox();
-    const inboxes = await context.authInfo.inbox.listInboxes();
-    const pow = Math.floor(Math.random() * (6 - 1) + 1);
-    const multiplier = Math.pow(10, pow);
-    const data = inboxes.map((inbox) => ({
-      inbox: inbox.name,
-      pending: Math.floor(Math.random() * multiplier),
-      investigating: Math.floor(Math.random() * multiplier),
-      closed: Math.floor(Math.random() * multiplier),
-      snoozed: Math.floor(Math.random() * multiplier),
-    }));
+    const data = await context.authInfo.analytics.getCaseStatusByInbox();
 
     return { caseStatusByInbox: data };
   },
