c3a2aa8b768984083e96df58fabc1f512424d43e
Sinan <daglarsinan@gmail.com>
2025-12-09 11:39:44 +0100

    feat(cases/inbox): add favorite inbox    functionality
       Allow users to mark an inbox as favorite. When navigating to
       /cases/inboxes,
       users are automatically redirected to their favorite inbox instead of
       the
       default My inbox. The favorite is persisted in a browser cookie.

--- packages/app-builder/src/routes/_builder+/cases+/inboxes.index.tsx
+++ packages/app-builder/src/routes/_builder+/cases+/inboxes.index.tsx
@@ -1,9 +1,12 @@
 import { MY_INBOX_ID } from '@app-builder/constants/inboxes';
 import { createServerFn } from '@app-builder/core/requests';
 import { authMiddleware } from '@app-builder/middlewares/auth-middleware';
+import { getPreferencesCookie } from '@app-builder/utils/preferences-cookies/preferences-cookie-read.server';
 import { getRoute } from '@app-builder/utils/routes';
 import { redirect } from '@remix-run/server-runtime';
 
-export const loader = createServerFn([authMiddleware], async function casesInboxesLayoutLoader({ context }) {
-  throw redirect(getRoute('/cases/inboxes/:inboxId', { inboxId: MY_INBOX_ID }));
+export const loader = createServerFn([authMiddleware], async function casesInboxesLayoutLoader({ request }) {
+  const favoriteInboxId = getPreferencesCookie(request, 'favInbox');
+  const targetInboxId = favoriteInboxId || MY_INBOX_ID;
+  throw redirect(getRoute('/cases/inboxes/:inboxId', { inboxId: targetInboxId }));
 });
