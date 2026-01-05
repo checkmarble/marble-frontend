4d94de8846d4d4a4ae7f5f6fe5b512b9f216ddd7
Sinan <daglarsinan@gmail.com>
2026-01-05 16:52:17 +0100

    feat: few feedback fixes

--- packages/app-builder/src/components/AstBuilder/styles/RemoveButton.tsx
+++ packages/app-builder/src/components/AstBuilder/styles/RemoveButton.tsx
@@ -8,7 +8,7 @@ export const RemoveButton = React.forwardRef<HTMLButtonElement, ButtonProps>(({ 
     <button
       className={clsx(
         'size-fit rounded-xs border p-1 text-xs transition-colors duration-200 ease-in-out',
-        'bg-surface-card text-grey-secondary border-grey-primary',
+        'bg-surface-card text-grey-secondary border-grey-border',
         'hover:text-grey-white hover:border-red-primary hover:bg-red-primary',
         'active:bg-red-hover active:border-red-hover',
         className,
