bcffb57e615b68859edb64604bbbdb2ebef9c432
Sinan <daglarsinan@gmail.com>
2026-01-13 10:24:07 +0100

    fix: change layout

--- packages/app-builder/src/components/Screenings/FreeformSearch/ThresholdSlider.tsx
+++ packages/app-builder/src/components/Screenings/FreeformSearch/ThresholdSlider.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import { type FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'ui-design-system';
 
 import { screeningsI18n } from '../screenings-i18n';
 
@@ -14,46 +15,31 @@ export const ThresholdSlider: FunctionComponent<ThresholdSliderProps> = ({ value
   const { t } = useTranslation(screeningsI18n);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(Number(e.target.value));
-  };
-
-  const handleClear = () => {
-    onChange(undefined);
+    const newValue = e.target.value;
+    if (newValue === '') {
+      onChange(undefined);
+      return;
+    }
+    const numValue = parseInt(newValue, 10);
+    if (isNaN(numValue)) {
+      onChange(undefined);
+      return;
+    }
+    if (numValue < 0) {
+      onChange(0);
+      return;
+    }
+    if (numValue > 100) {
+      onChange(100);
+      return;
+    }
+    onChange(numValue);
   };
 
   return (
     <div className={clsx('flex flex-col gap-2', className)}>
-      <div className="flex items-center justify-between">
-        <label className="text-s font-medium text-grey-primary">
-          {t('screenings:freeform_search.threshold_label')}
-        </label>
-        {value !== undefined && (
-          <button
-            type="button"
-            onClick={handleClear}
-            className="text-s text-purple-primary hover:text-purple-primary/80"
-          >
-            {t('screenings:freeform_search.clear')}
-          </button>
-        )}
-      </div>
-      <div className="flex items-center gap-4">
-        <input
-          type="range"
-          min={0}
-          max={100}
-          step={1}
-          value={value ?? 50}
-          onChange={handleChange}
-          className={clsx(
-            'h-2 w-full cursor-pointer appearance-none rounded-full',
-            'bg-grey-background-light dark:bg-grey-background-light',
-            '[&::-webkit-slider-thumb]:size-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-primary',
-            '[&::-moz-range-thumb]:size-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:bg-purple-primary',
-          )}
-        />
-        <span className="text-s min-w-[3ch] text-right font-semibold text-grey-primary">{value ?? '-'}</span>
-      </div>
+      <label className="text-s font-medium text-grey-primary">{t('screenings:freeform_search.threshold_label')}</label>
+      <Input type="number" value={value ?? ''} onChange={handleChange} min={0} max={100} placeholder="0-100" />
       <p className="text-xs text-grey-placeholder">{t('screenings:freeform_search.threshold_description')}</p>
     </div>
   );
--- packages/app-builder/src/routes/_builder+/screening-search+/_index.tsx
+++ packages/app-builder/src/routes/_builder+/screening-search+/_index.tsx
@@ -1,12 +1,24 @@
 import { ErrorComponent, Page } from '@app-builder/components';
 import { BreadCrumbs } from '@app-builder/components/Breadcrumbs';
-import { FreeformSearchPage } from '@app-builder/components/Screenings/FreeformSearch/FreeformSearchPage';
+import { PrintHeader, PrintView } from '@app-builder/components/Print';
+import {
+  FreeformSearchPage,
+  type FreeformSearchState,
+} from '@app-builder/components/Screenings/FreeformSearch/FreeformSearchPage';
+import {
+  PrintResults,
+  PrintSearchSummary,
+} from '@app-builder/components/Screenings/FreeformSearch/FreeformSearchPrint';
 import { createServerFn } from '@app-builder/core/requests';
 import { authMiddleware } from '@app-builder/middlewares/auth-middleware';
+import { useOrganizationDetails } from '@app-builder/services/organization/organization-detail';
 import { useRouteError } from '@remix-run/react';
 import { captureRemixErrorBoundaryError } from '@sentry/remix';
 import { type Namespace } from 'i18next';
+import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ButtonV2 } from 'ui-design-system';
+import { Icon } from 'ui-icons';
 
 export const handle = {
   i18n: ['common', 'screenings', 'navigation'] satisfies Namespace,
@@ -18,16 +30,41 @@ export const loader = createServerFn([authMiddleware], async () => {
 
 export default function ScreeningSearchIndexPage() {
   const { t } = useTranslation(['screenings']);
+  const { currentUser } = useOrganizationDetails();
+  const [searchState, setSearchState] = useState<FreeformSearchState | null>(null);
+
+  const userName = [currentUser.actorIdentity.firstName, currentUser.actorIdentity.lastName].filter(Boolean).join(' ');
+
+  const handleSearchComplete = useCallback((state: FreeformSearchState) => {
+    setSearchState(state);
+  }, []);
+
+  const hasResults = searchState !== null && searchState.results.length > 0;
 
   return (
     <Page.Main>
-      <Page.Header>
+      <Page.Header className="justify-between">
         <BreadCrumbs />
+        {hasResults && (
+          <PrintView
+            title={t('screenings:print.title')}
+            trigger={
+              <ButtonV2 variant="secondary">
+                <Icon icon="download" className="size-4" />
+                {t('screenings:print.open_print_view')}
+              </ButtonV2>
+            }
+          >
+            <PrintHeader title={t('screenings:print.title')} userName={userName} />
+            <PrintSearchSummary searchInputs={searchState.inputs} />
+            <PrintResults results={searchState.results} />
+          </PrintView>
+        )}
       </Page.Header>
       <Page.Container>
         <Page.Description>{t('screenings:freeform_search.description')}</Page.Description>
         <Page.ContentV2 className="h-full">
-          <FreeformSearchPage />
+          <FreeformSearchPage onSearchComplete={handleSearchComplete} />
         </Page.ContentV2>
       </Page.Container>
     </Page.Main>
