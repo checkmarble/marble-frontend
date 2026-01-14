808270fde79585624808e0c59e68e014ba374801
Sinan <daglarsinan@gmail.com>
2026-01-14 17:33:07 +0100

    fixes

--- packages/app-builder/src/routes/_builder+/data+/_layout.tsx
+++ packages/app-builder/src/routes/_builder+/data+/_layout.tsx
@@ -1,5 +1,7 @@
 import { Page } from '@app-builder/components';
 import { BreadCrumbLink, type BreadCrumbProps, BreadCrumbs } from '@app-builder/components/Breadcrumbs';
+import { createServerFn } from '@app-builder/core/requests';
+import { authMiddleware } from '@app-builder/middlewares/auth-middleware';
 import { DataModelContextProvider } from '@app-builder/services/data/data-model';
 import {
   isCreateDataModelFieldAvailable,
@@ -10,9 +12,7 @@ import {
   isEditDataModelInfoAvailable,
   isIngestDataAvailable,
 } from '@app-builder/services/feature-access';
-import { initServerServices } from '@app-builder/services/init.server';
 import { getRoute } from '@app-builder/utils/routes';
-import { json, type LoaderFunctionArgs } from '@remix-run/node';
 import { NavLink, Outlet, useLoaderData } from '@remix-run/react';
 import { type Namespace } from 'i18next';
 import { useTranslation } from 'react-i18next';
@@ -35,14 +35,11 @@ export const handle = {
   ],
 };
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const { authService } = initServerServices(request);
-  const { user, dataModelRepository } = await authService.isAuthenticated(request, {
-    failureRedirect: getRoute('/sign-in'),
-  });
+export const loader = createServerFn([authMiddleware], async function dataLayout({ context }) {
+  const { user, dataModelRepository } = context.authInfo;
 
   const dataModel = await dataModelRepository.getDataModel();
-  return json({
+  return {
     dataModel,
     dataModelFeatureAccess: {
       isCreateDataModelTableAvailable: isCreateDataModelTableAvailable(user),
@@ -53,8 +50,8 @@ export async function loader({ request }: LoaderFunctionArgs) {
       isCreateDataModelPivotAvailable: isCreateDataModelPivotAvailable(user),
       isIngestDataAvailable: isIngestDataAvailable(user),
     },
-  });
-}
+  };
+});
 
 export default function Data() {
   const { t } = useTranslation(handle.i18n);
