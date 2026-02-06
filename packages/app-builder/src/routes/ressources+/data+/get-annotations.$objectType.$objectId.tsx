d668843ab32947ff0307bdb94e04491df69b0345
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-02-06 10:09:18 +0100

    Load file annotatin thumbnails on the Client 360 page.

--- packages/app-builder/src/routes/ressources+/data+/get-annotations.$objectType.$objectId.tsx
+++ packages/app-builder/src/routes/ressources+/data+/get-annotations.$objectType.$objectId.tsx
@@ -2,17 +2,33 @@ import { createServerFn, data } from '@app-builder/core/requests';
 import { authMiddleware } from '@app-builder/middlewares/auth-middleware';
 import { handleRedirectMiddleware } from '@app-builder/middlewares/handle-redirect-middleware';
 import invariant from 'tiny-invariant';
+import z from 'zod';
 
-export const loader = createServerFn([handleRedirectMiddleware, authMiddleware], async ({ context, params }) => {
-  const { objectType, objectId } = params;
-  const { dataModelRepository } = context.authInfo;
-
-  invariant(objectType, 'Object type is required');
-  invariant(objectId, 'Object ID is required');
-
-  const annotations = await dataModelRepository.getAnnotationsByTableNameAndObjectId(objectType, objectId);
-
-  console.log('annotations', annotations);
-
-  return data({ annotations });
+const queryParams = z.object({
+  load_thumbnails: z.stringbool().optional(),
 });
+
+export const loader = createServerFn(
+  [handleRedirectMiddleware, authMiddleware],
+  async ({ request, context, params }) => {
+    const { objectType, objectId } = params;
+    const { dataModelRepository } = context.authInfo;
+
+    const url = new URL(request.url);
+    const searchParams = new URLSearchParams(url.search);
+    const parsedSearchParams = queryParams.parse(Object.fromEntries(searchParams));
+
+    invariant(objectType, 'Object type is required');
+    invariant(objectId, 'Object ID is required');
+
+    const annotations = await dataModelRepository.getAnnotationsByTableNameAndObjectId(
+      objectType,
+      objectId,
+      parsedSearchParams.load_thumbnails ?? false,
+    );
+
+    console.log('annotations', annotations);
+
+    return data({ annotations });
+  },
+);
