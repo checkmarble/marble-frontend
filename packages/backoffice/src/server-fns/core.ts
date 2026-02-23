00653280aa9c41c56d05f9b969abe78d99ad5861
Christopher Debove <christopher@checkmarble.com>
2026-02-23 13:53:18 +0100

    ci(backoffice): create backoffice standalone image

--- packages/backoffice/src/server-fns/core.ts
+++ packages/backoffice/src/server-fns/core.ts
@@ -3,6 +3,7 @@ import { createServerFn } from '@tanstack/react-start';
 import { marblecoreApi } from 'marble-api';
 
 export const getAppConfigFn = createServerFn({ method: 'GET' }).handler(async () => {
+  console.log('GET APP CONFIG FN', env.API_BASE_URL);
   const appConfig = await marblecoreApi.getAppConfig({ baseUrl: env.API_BASE_URL });
   return appConfig;
 });
--- packages/backoffice/tailwind.config.ts
+++ packages/backoffice/tailwind.config.ts
@@ -1,3 +1,3 @@
 export default {
-  content: ['./src/**/*.{ts,tsx,jsx,js}', '../ui-design-system/src/**/*.{ts,tsx,jsx,js}'],
+  content: ['./src/**/*.{ts,tsx,jsx,js}', '../ui-design-system/src/**/*.{ts,tsx,jsx,js}', '!.output/**', '!.nitro/**'],
 };
