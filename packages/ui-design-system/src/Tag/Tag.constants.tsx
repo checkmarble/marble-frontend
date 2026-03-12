e9e8f9e9ecc4d25843ed8d2aeed564c1ee43b071
Christopher Debove <christopher@checkmarble.com>
2026-03-12 10:31:19 +0100

    feat: rework tag to be outlined on light mode

--- packages/ui-design-system/src/Tag/Tag.constants.tsx
+++ packages/ui-design-system/src/Tag/Tag.constants.tsx
@@ -1,3 +1,2 @@
 export const tagColors = ['purple', 'blue', 'green', 'red', 'yellow', 'orange', 'grey', 'grey-light'] as const;
-export const tagSize = ['small', 'big'] as const;
-export const tagBorder = ['rounded-sm', 'square'] as const;
+export const tagSize = ['small', 'medium', 'big'] as const;
