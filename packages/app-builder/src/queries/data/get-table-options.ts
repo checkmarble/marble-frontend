98b3b64a7775123d80c9d88651636633a2b6e04e
William Schlegel <william.schlegel@gmail.com>
2026-03-23 14:32:38 +0100

    simplify the hook

--- packages/app-builder/src/queries/data/get-table-options.ts
+++ packages/app-builder/src/queries/data/get-table-options.ts
@@ -5,17 +5,11 @@ import { useQuery } from '@tanstack/react-query';
 
 const endpoint = (tableId: string) => getRoute('/ressources/data/:tableId/table-options', { tableId });
 
-export function tableOptionsQueryOptions(tableId: string) {
-  return {
-    queryKey: ['data-model', 'table-options', tableId],
-  };
-}
-
 export function useTableOptionsQuery(tableId: string | undefined) {
   const navigate = useAgnosticNavigation();
   if (!tableId) return null;
   return useQuery({
-    ...tableOptionsQueryOptions(tableId),
+    queryKey: ['data-model', 'table-options', tableId],
     queryFn: async () => {
       const res = await fetch(endpoint(tableId));
       const result = await res.json();
