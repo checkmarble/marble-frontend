9dcd9593062df9cae90c361d07cb7cce396a64a7
William Schlegel <william.schlegel@gmail.com>
2026-04-15 15:08:36 +0200

    use new endpoint body schema for table creation

--- packages/tests/e2e/fixtures.ts
+++ packages/tests/e2e/fixtures.ts
@@ -7,15 +7,14 @@ interface Org {
 
 interface Table {
   table: string;
-  fields: { [key: string]: string };
+  fields: { [key: string]: { type: string; semantic_type: string } };
 }
-
 const TABLES: Table[] = [
   {
     table: 'transactions',
     fields: {
-      beneficiary: 'String',
-      amount: 'Float',
+      beneficiary: { type: 'String', semantic_type: 'name' },
+      amount: { type: 'Float', semantic_type: 'monetary_amount' },
     },
   },
 ];
@@ -42,30 +41,62 @@ export const setupFixtures = async (dsn: string, apiUrl: string) => {
 };
 
 const createTable = async (apiUrl: string, apiKey: string, table: Table) => {
+  const bodyValues = {
+    name: table.table,
+    primary_ordering_field: 'updated_at',
+    semantic_type: 'other',
+    description: 'lorem ipsum',
+    fields: [
+      {
+        alias: 'object_id',
+        description: '',
+        is_enum: false,
+        is_unique: false,
+        metadata: {
+          hidden: false,
+          semanticSubType: 'opaque_id',
+          semanticTypeForFront: 'unique_id',
+        },
+        name: 'object_id',
+        nullable: false,
+        semantic_type: 'id',
+        type: 'String',
+      },
+      {
+        alias: 'updated_at',
+        description: '',
+        is_enum: false,
+        is_unique: false,
+        metadata: {
+          hidden: false,
+          semanticTypeForFront: 'last_update',
+        },
+        name: 'updated_at',
+        nullable: false,
+        semantic_type: 'last_update',
+        type: 'Timestamp',
+      },
+      ...Object.entries(table.fields).map(([key, field]) => ({
+        alias: key,
+        description: '',
+        is_enum: false,
+        is_unique: false,
+        name: key,
+        nullable: true,
+        semantic_type: field.semantic_type,
+        type: field.type,
+      })),
+    ],
+    links: [],
+  };
+
   const tableResponse = await fetch(`${apiUrl}/data-model/tables`, {
     method: 'POST',
     headers: {
       'x-api-key': apiKey,
     },
-    body: JSON.stringify({ name: table.table, description: 'Lorem ipsum.' }),
+    body: JSON.stringify(bodyValues),
   });
 
   if (tableResponse.status != 200) throw new Error('failed to create data model table');
-
-  const tableId = ((await tableResponse.json()) as { id: string }).id;
-
-  for (const [fieldName, fieldType] of Object.entries(table.fields)) {
-    const fieldResponse = await fetch(`${apiUrl}/data-model/tables/${tableId}/fields`, {
-      method: 'POST',
-      headers: {
-        'x-api-key': apiKey,
-      },
-      body: JSON.stringify({
-        name: fieldName,
-        type: fieldType,
-      }),
-    });
-
-    if (fieldResponse.status != 200) throw new Error('failed to create data model table field');
-  }
 };
