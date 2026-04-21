7afe47286219d13be8d667d5b1a1420153a7fb7a
Pascal Delange <pascal@checkmarble.com>
2026-04-21 11:59:05 +0200

    fix filters in audit logs page

--- packages/app-builder/src/routes/_app/_builder/settings/audit-logs.tsx
+++ packages/app-builder/src/routes/_app/_builder/settings/audit-logs.tsx
@@ -3,9 +3,7 @@ import { authMiddleware } from '@app-builder/middlewares/auth-middleware';
 import { isAdmin } from '@app-builder/models';
 import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/react-start';
-import { getRequest } from '@tanstack/react-start/server';
-import QueryString from 'qs';
-import { z } from 'zod';
+import { z } from 'zod/v4';
 
 const DEFAULT_LIMIT = 25;
 
@@ -14,31 +12,33 @@ const pageQueryStringSchema = z.object({
   limit: z.coerce.number().optional().default(DEFAULT_LIMIT),
 });
 
+const activityFollowUpLoaderSchema = z.object({
+  query: pageQueryStringSchema,
+});
+
 const activityFollowUpLoader = createServerFn()
   .middleware([authMiddleware])
-  .handler(async function activityFollowUpLoader({ context }) {
-    const request = getRequest();
+  .inputValidator(activityFollowUpLoaderSchema)
+  .handler(async function activityFollowUpLoader({ context, data: { query } }) {
     const { user, apiKey } = context.authInfo;
 
     if (!isAdmin(user)) {
       throw redirect({ to: '/' });
     }
 
-    const url = new URL(request.url);
-    const searchParams = new URLSearchParams(url.search);
-    const parsedSearchParams = pageQueryStringSchema.parse(Object.fromEntries(searchParams));
-
     const apiKeys = await apiKey.listApiKeys();
 
     return {
-      query: parsedSearchParams.q,
-      limit: parsedSearchParams.limit,
+      query: query.q,
+      limit: query.limit,
       apiKeys,
     };
   });
 
 export const Route = createFileRoute('/_app/_builder/settings/audit-logs')({
-  loader: () => activityFollowUpLoader(),
+  validateSearch: pageQueryStringSchema,
+  loaderDeps: ({ search }) => search,
+  loader: ({ deps }) => activityFollowUpLoader({ data: { query: deps } }),
   component: ActivityFollowUp,
 });
 
@@ -47,14 +47,14 @@ function ActivityFollowUp() {
   const { query, limit, apiKeys } = Route.useLoaderData();
 
   const updatePage = (newQuery: string, newLimit: number) => {
-    const qs = QueryString.stringify(
-      {
+    navigate({
+      to: '.',
+      search: {
         q: newQuery !== '' ? newQuery : undefined,
         limit: newLimit !== DEFAULT_LIMIT ? newLimit : undefined,
       },
-      { addQueryPrefix: true, skipNulls: true },
-    );
-    navigate({ to: `/settings/audit-logs${qs}`, replace: true });
+      replace: true,
+    });
   };
 
   return <ActivityFollowUpPage query={query} limit={limit} updatePage={updatePage} apiKeys={apiKeys} />;
