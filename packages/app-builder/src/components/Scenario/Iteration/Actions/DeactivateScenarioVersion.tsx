c1ebb504d25bbc19e2a5c086df0117be95263c4f
William Schlegel <william.schlegel@gmail.com>
2026-06-10 12:07:42 +0200

    refactor deactivate modal

--- packages/app-builder/src/components/Scenario/Iteration/Actions/DeactivateScenarioVersion.tsx
+++ packages/app-builder/src/components/Scenario/Iteration/Actions/DeactivateScenarioVersion.tsx
@@ -1,19 +1,14 @@
-import { FormLabel } from '@app-builder/components/Form/Tanstack/FormLabel';
 import { useLoaderRevalidator } from '@app-builder/contexts/LoaderRevalidatorContext';
-import {
-  deactivateIterationPayloadSchema,
-  useDeactivateIterationMutation,
-} from '@app-builder/queries/scenarios/deactivate-iteration';
-import { useForm } from '@tanstack/react-form';
-import * as React from 'react';
+import { useDeactivateIterationMutation } from '@app-builder/queries/scenarios/deactivate-iteration';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
-import { Button, Checkbox, Modal } from 'ui-design-system';
+import { Button, Modal } from 'ui-design-system';
 import { Icon } from 'ui-icons';
 
 export function DeactivateScenarioVersion({ scenarioId, iterationId }: { scenarioId: string; iterationId: string }) {
   const { t } = useTranslation(['scenarios']);
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
     <Modal.Root open={open} onOpenChange={setOpen}>
@@ -35,81 +30,28 @@ function DeactivateScenarioVersionContent({ scenarioId, iterationId }: { scenari
   const deactivateIterationMutation = useDeactivateIterationMutation(scenarioId, iterationId);
   const revalidate = useLoaderRevalidator();
 
-  const form = useForm({
-    defaultValues: {
-      stopOperating: false,
-      changeIsImmediate: false,
-    },
-    onSubmit: ({ value, formApi }) => {
-      if (formApi.state.isValid) {
-        const payload = deactivateIterationPayloadSchema.safeParse(value);
-        if (payload.success) {
-          deactivateIterationMutation
-            .mutateAsync(payload.data)
-            .then(() => {
-              revalidate();
-            })
-            .catch(() => {
-              toast.error(t('common:errors.unknown'));
-            });
-        }
-      }
-    },
-    validators: {
-      onSubmitAsync: deactivateIterationPayloadSchema,
-    },
-  });
+  function handleDeactivate() {
+    deactivateIterationMutation
+      .mutateAsync({ stopOperating: true, changeIsImmediate: true })
+      .then(() => {
+        toast.success(t('scenarios:deployment_modal.deactivate.success'));
+        revalidate();
+      })
+      .catch(() => {
+        toast.error(t('common:errors.unknown'));
+      });
+  }
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
-      }}
-    >
+    <>
       <Modal.Title>{t('scenarios:deployment_modal.deactivate.title')}</Modal.Title>
       <div className="flex flex-col gap-6 p-6">
         <div className="text-s flex flex-col gap-4 font-medium">
           <p className="font-semibold">{t('scenarios:deployment_modal.deactivate.confirm')}</p>
-          <form.Field
-            name="stopOperating"
-            validators={{
-              onBlur: deactivateIterationPayloadSchema.shape.stopOperating,
-              onChange: deactivateIterationPayloadSchema.shape.stopOperating,
-            }}
-          >
-            {(field) => (
-              <div className="group flex flex-row items-center gap-2">
-                <Checkbox
-                  name={field.name}
-                  defaultChecked={field.state.value}
-                  onCheckedChange={(state) => state !== 'indeterminate' && field.handleChange(state)}
-                />
-                <FormLabel name={field.name}>{t('scenarios:deployment_modal.deactivate.stop_operating')}</FormLabel>
-              </div>
-            )}
-          </form.Field>
-          <form.Field
-            name="changeIsImmediate"
-            validators={{
-              onBlur: deactivateIterationPayloadSchema.shape.changeIsImmediate,
-              onChange: deactivateIterationPayloadSchema.shape.changeIsImmediate,
-            }}
-          >
-            {(field) => (
-              <div className="group flex flex-row items-center gap-2">
-                <Checkbox
-                  name={field.name}
-                  defaultChecked={field.state.value}
-                  onCheckedChange={(state) => state !== 'indeterminate' && field.handleChange(state)}
-                />
-                <FormLabel name={field.name}>
-                  {t('scenarios:deployment_modal.deactivate.change_is_immediate')}
-                </FormLabel>
-              </div>
-            )}
-          </form.Field>
+          <ul className="flex list-disc flex-col gap-4 ps-5">
+            <li>{t('scenarios:deployment_modal.deactivate.stop_operating')}</li>
+            <li>{t('scenarios:deployment_modal.deactivate.change_is_immediate')}</li>
+          </ul>
           <p className="text-grey-disabled text-xs font-medium">{t('scenarios:deployment_modal.deactivate.helper')}</p>
         </div>
       </div>
@@ -119,11 +61,11 @@ function DeactivateScenarioVersionContent({ scenarioId, iterationId }: { scenari
             {t('common:cancel')}
           </Button>
         </Modal.Close>
-        <Button className="flex-1" variant="destructive" type="submit">
+        <Button className="flex-1" variant="destructive" onClick={handleDeactivate}>
           <Icon icon="stop" className="size-5" />
           {t('scenarios:deployment_modal.deactivate.button')}
         </Button>
       </Modal.Footer>
-    </form>
+    </>
   );
 }
