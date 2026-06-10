1824c453bf77b27233beba76bc7b94f649e18f99
Pascal Delange <pascal@checkmarble.com>
2026-06-10 22:24:51 +0200

    fix csp headers ?

--- packages/app-builder/src/start.ts
+++ packages/app-builder/src/start.ts
@@ -1,8 +1,10 @@
 import { createStart } from '@tanstack/react-start';
 import { convertRedirectErrorToExceptionMiddleware } from './middlewares/globals';
+import { securityHeadersMiddleware } from './middlewares/security-headers';
 
 export const startInstance = createStart(() => {
   return {
+    requestMiddleware: [securityHeadersMiddleware],
     functionMiddleware: [convertRedirectErrorToExceptionMiddleware],
   };
 });
