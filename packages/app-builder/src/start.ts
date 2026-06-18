05904d8362c9a11142bddbf00b18ed8e283cc275
Pascal Delange <pascal@checkmarble.com>
2026-06-18 15:52:11 +0200

    middleware to redirect long uuids to short uuids

--- packages/app-builder/src/start.ts
+++ packages/app-builder/src/start.ts
@@ -1,10 +1,11 @@
 import { createStart } from '@tanstack/react-start';
 import { convertRedirectErrorToExceptionMiddleware } from './middlewares/globals';
 import { securityHeadersMiddleware } from './middlewares/security-headers';
+import { shortUUIDRedirectMiddleware } from './middlewares/short-uuid-redirect';
 
 export const startInstance = createStart(() => {
   return {
-    requestMiddleware: [securityHeadersMiddleware],
+    requestMiddleware: [shortUUIDRedirectMiddleware, securityHeadersMiddleware],
     functionMiddleware: [convertRedirectErrorToExceptionMiddleware],
   };
 });
