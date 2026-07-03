9fb1c453040e00afed4ec82aa2d9e910efc607c5
William Schlegel <william.schlegel@gmail.com>
2026-07-03 15:38:47 +0200

    observalbility  dataset update

--- packages/app-builder/src/components/ContinuousScreening/ObservabilityPage.tsx
+++ packages/app-builder/src/components/ContinuousScreening/ObservabilityPage.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next';
 import { Card, cn, DefaultTooltip, Tag, Typo } from 'ui-design-system';
 import { Icon } from 'ui-icons';
 import { Callout } from '../Callout';
+import GridTable from '../GridTable';
 import { pageLayoutGutter } from '../Page/page-layout';
 import { Panel } from '../Panel';
 
@@ -13,7 +14,7 @@ export function ObservabilityPage() {
   return (
     <Page.Main>
       <Page.Content>
-        <div className={cn('flex flex-col gap-lg', pageLayoutGutter.gap)}>
+        <div className={cn('flex flex-col', pageLayoutGutter.gap)}>
           <ScreeningNavigationTabs />
           <div className={cn('grid lg:grid-cols-2', pageLayoutGutter.gap)}>
             <Card className="p-md grid gap-sm ">
@@ -38,13 +39,120 @@ export function ObservabilityPage() {
                 ]}
               />
             </Card>
-            <div></div>
+            <Card className="p-md grid gap-sm ">
+              <header className="flex justify-between items-center">
+                <div className="flex gap-xs">
+                  <Typo variant="title2">Dataset updates</Typo>
+                  <DefaultTooltip content="Dataset updates are the process of updating the dataset with new data.">
+                    <Icon icon="tip" className="size-4" />
+                  </DefaultTooltip>
+                </div>
+                <PanelDatasetUpdate />
+              </header>
+              <Callout color="purple">Datasets from provider and their status in Marble.</Callout>
+              <DatasetUpdate
+                data={[
+                  { updateDate: '2026-07-03T12:00:00Z', updateVersion: '0230240055', updateStatus: 'pending' },
+                  { updateDate: '2026-07-02T12:00:00Z', updateVersion: '0230240054', updateStatus: 'completed' },
+                  { updateDate: '2026-07-01T12:00:00Z', updateVersion: '0230240053', updateStatus: 'failed' },
+                ]}
+              />
+            </Card>
+            <GridVersions
+              data={[
+                {
+                  version: '0230240055',
+                  receptionTime: '2026-07-03T12:00:00Z',
+                  jobsStart: '2026-07-03T12:00:00Z',
+                  jobsEnd: '2026-07-03T12:00:00Z',
+                  configurationName: 'Configuration 1',
+                  status: 'inProgress',
+                  progressValue: 50,
+                },
+                {
+                  version: '0230240054',
+                  receptionTime: '2026-07-02T12:00:00Z',
+                  jobsStart: '2026-07-02T12:00:00Z',
+                  jobsEnd: '2026-07-02T12:00:00Z',
+                  configurationName: 'Configuration 2',
+                  status: 'completed',
+                  progressValue: 100,
+                },
+                {
+                  version: '0230240053',
+                  receptionTime: '2026-07-01T12:00:00Z',
+                  jobsStart: '2026-07-01T12:00:00Z',
+                  jobsEnd: '2026-07-01T12:00:00Z',
+                  configurationName: 'Configuration 3',
+                  status: 'failed',
+                  progressValue: 0,
+                },
+              ]}
+            />
           </div>
-          <div></div>
         </div>
       </Page.Content>
     </Page.Main>
   );
+}
+
+type GridVersionsProps = {
+  data: {
+    version: string;
+    receptionTime: string;
+    jobsStart: string;
+    jobsEnd: string;
+    configurationName: string;
+    status: 'inProgress' | 'completed' | 'failed';
+    progressValue: number;
+  }[];
+};
+
+function GridVersions({ data }: GridVersionsProps) {
+  const locale = useFormatLanguage();
+  const timezone = useFormatTimezone();
+
+  return (
+    <div className="col-span-2 flex flex-col gap-sm">
+      <Callout color="purple" className="bg-surface-card w-fit" bordered>
+        Versions of dataset in Marble.
+      </Callout>
+      <GridTable.Table className="grid-cols-6">
+        <GridTable.Row className="font-semibold border-b border-grey-border">
+          <GridTable.Cell>Dataset version</GridTable.Cell>
+          <GridTable.Cell>Reception time</GridTable.Cell>
+          <GridTable.Cell>Jobs’ start</GridTable.Cell>
+          <GridTable.Cell>Jobs’ end</GridTable.Cell>
+          <GridTable.Cell>Name of configuration</GridTable.Cell>
+          <GridTable.Cell>Status</GridTable.Cell>
+        </GridTable.Row>
+        {data.map((item) => (
+          <GridTable.Row key={item.version}>
+            <GridTable.Cell>{item.version}</GridTable.Cell>
+            <GridTable.Cell>{formatDateAtTime(item.receptionTime, { locale, timeZone: timezone })}</GridTable.Cell>
+            <GridTable.Cell>{formatDateAtTime(item.jobsStart, { locale, timeZone: timezone })}</GridTable.Cell>
+            <GridTable.Cell>{formatDateAtTime(item.jobsEnd, { locale, timeZone: timezone })}</GridTable.Cell>
+            <GridTable.Cell>{item.configurationName}</GridTable.Cell>
+            <GridTable.Cell>
+              <GridStatus status={item.status} progressValue={item.progressValue} />
+            </GridTable.Cell>
+          </GridTable.Row>
+        ))}
+      </GridTable.Table>
+    </div>
+  );
+}
+
+function GridStatus({
+  status,
+  progressValue,
+}: {
+  status: GridVersionsProps['data'][number]['status'];
+  progressValue: number;
+}) {
+  if (status === 'completed') return <Tag color="green">Completed</Tag>;
+  if (status === 'failed') return <Tag color="red">Failed</Tag>;
+  return <Tag color="yellow">{`In progress - ${progressValue}%`}</Tag>;
 }
 
 type ClientDataIndexingProps = {
@@ -101,10 +209,91 @@ function ClientDataIndexing({ data }: ClientDataIndexingProps) {
   );
 }
 
+type DatasetUpdateProps = {
+  data: {
+    updateDate: string;
+    updateVersion: string;
+    updateStatus: 'pending' | 'completed' | 'failed';
+  }[];
+};
+
+function DatasetUpdate({ data }: DatasetUpdateProps) {
+  const { t } = useTranslation(['continuousScreening']);
+  const locale = useFormatLanguage();
+  const timezone = useFormatTimezone();
+
+  return (
+    <div className="grid grid-cols-2 gap-md w-fit">
+      {data.map((item) => {
+        const formattedDate = formatDateAtTime(item.updateDate, {
+          locale,
+          timeZone: timezone,
+          todayLabel: t('continuousScreening:observability.today'),
+          yesterdayLabel: t('continuousScreening:observability.yesterday'),
+          atSeparator: t('continuousScreening:observability.date_time_separator'),
+        });
+
+        return (
+          <div key={item.updateDate} className="grid col-span-full grid-cols-subgrid items-center">
+            <time dateTime={item.updateDate} className="text-grey-secondary">
+              {formattedDate}
+            </time>
+            <Tag
+              color={item.updateStatus === 'pending' ? 'yellow' : item.updateStatus === 'completed' ? 'green' : 'red'}
+              className="gap-sm"
+            >
+              <Icon
+                icon={
+                  item.updateStatus === 'pending'
+                    ? 'waiting_for_action'
+                    : item.updateStatus === 'completed'
+                      ? 'checked'
+                      : 'x'
+                }
+                className="size-4"
+              />
+              v.{item.updateVersion}
+            </Tag>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
 function PanelCientIndexing() {
   return (
     <Panel.Root>
-      <Icon icon="eye" className="size-4" />
+      <Panel.Trigger>
+        <Icon icon="eye" className="size-4" />
+      </Panel.Trigger>
+      <Panel.Container size="small">
+        <Panel.Content>
+          <Panel.Header>
+            <Typo variant="title2">Client data indexing</Typo>
+          </Panel.Header>
+          <Panel.Footer>
+            <Panel.FooterButton label="Close" isCloseButton />
+          </Panel.Footer>
+        </Panel.Content>
+      </Panel.Container>
+    </Panel.Root>
+  );
+}
+
+function PanelDatasetUpdate() {
+  return (
+    <Panel.Root>
+      <Panel.Trigger>
+        <Icon icon="eye" className="size-4" />
+      </Panel.Trigger>
+      <Panel.Container size="small">
+        <Panel.Content>
+          <Panel.Header>
+            <Typo variant="title2">Dataset updates</Typo>
+          </Panel.Header>
+        </Panel.Content>
+      </Panel.Container>
     </Panel.Root>
   );
 }
