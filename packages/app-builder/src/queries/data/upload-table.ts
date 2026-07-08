ac0ce4347ce0dab4c7dccca37d06455f3a0b990c
William Schlegel <william.schlegel@gmail.com>
2026-07-08 10:05:03 +0200

    add refreshment of the query to monitor data ingestion

--- packages/app-builder/src/queries/data/upload-table.ts
+++ packages/app-builder/src/queries/data/upload-table.ts
@@ -2,12 +2,17 @@ import { getUploadLogsFn } from '@app-builder/server-fns/data';
 import { useQuery } from '@tanstack/react-query';
 import { useServerFn } from '@tanstack/react-start';
 
-export const useUploadTableQuery = (tableName: string, enabled: boolean) => {
+export const useUploadTableQuery = (
+  tableName: string,
+  enabled: boolean,
+  options: { refetchInterval?: number | false } = {},
+) => {
   const getUploadLogs = useServerFn(getUploadLogsFn);
 
   return useQuery({
     queryKey: ['ingestion', 'upload-logs', tableName],
     queryFn: async () => getUploadLogs({ data: { objectType: tableName } }),
     enabled,
+    refetchInterval: options.refetchInterval,
   });
 };
