d6c374bdd68b71384b445a63a573f2bbfdb6c76a
William Schlegel <william.schlegel@gmail.com>
2026-09-15 10:52:31 +0200

    Re-introduce the Investigation box in screening case

--- packages/app-builder/src/components/CaseManager/CaseInvestigation/CaseInvestigation.tsx
+++ packages/app-builder/src/components/CaseManager/CaseInvestigation/CaseInvestigation.tsx
@@ -3,18 +3,20 @@ import { CaseEvents } from '@app-builder/components/Cases/CaseEvents';
 import { CaseEvent } from '@app-builder/models/cases';
 import { RefObject } from 'react';
 import { useTranslation } from 'react-i18next';
+import { cn } from 'ui-design-system';
 
 type CaseInvestigationProps = {
   caseId: string;
   events: CaseEvent[];
   root: RefObject<HTMLDivElement | null>;
+  className?: string;
 };
 
-export const CaseInvestigation = ({ caseId, events, root }: CaseInvestigationProps) => {
+export const CaseInvestigation = ({ caseId, events, root, className }: CaseInvestigationProps) => {
   const { t } = useTranslation(['cases']);
 
   return (
-    <div className="flex flex-col justify-start gap-sm">
+    <div className={cn('flex flex-col justify-start gap-sm', className)}>
       <span className="text-default text-grey-primary px-2xs font-medium">{t('cases:investigation')}</span>
       <div className="border-grey-border bg-surface-card flex flex-col rounded-lg border overflow-hidden">
         <div className="p-md">
